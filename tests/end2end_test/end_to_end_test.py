29257c066740d56f87f4288295b8bd77289f8100
michael <m.owonibi@elifesciences.org>
2019-11-18 22:15:29 +0000

    #5183 ci pipeline end to end tests improved unit testing

--- tests/end2end_test/end_to_end_test.py
+++ tests/end2end_test/end_to_end_test.py
@@ -20,15 +20,7 @@ def test_dag_runs_data_imported():
     :return:
     """
     print("DAG about to tbe loaded")
-    try:
-        simple_query(
-            query=TestQueryTemplate.CLEAN_TABLE_QUERY,
-            project=PROJECT,
-            dataset=DATASET,
-            table=TABLE,
-        )
-    except Exception:
-        LOGGER.info("table not cleaned, maybe it does not exist")
+
     print("DAG about to tbe loaded step 2")
     dag_id = "Load_Crossref_Event_Into_Bigquery"
     config = {
@@ -39,8 +31,8 @@ def test_dag_runs_data_imported():
         "current_timestamp": "2012-10-01 00:00:00",
     }
     print("TAYOS",)
-    #execution_date = AIRFLW_API.trigger_dag(dag_id=dag_id, conf=config)
-    #print(execution_date)
+    execution_date = AIRFLW_API.trigger_dag(dag_id=dag_id, conf=config)
+    print(execution_date)
     #is_running = True
     #while is_running:
     #    is_running = AIRFLW_API.is_dag_running(dag_id, execution_date)
@@ -58,13 +50,6 @@ def test_dag_runs_data_imported():
     #assert query_response[0].get("count") > 0
     assert 1
 
-    # clean up
-    simple_query(
-        query=TestQueryTemplate.CLEAN_TABLE_QUERY,
-        project=PROJECT,
-        dataset=DATASET,
-        table=TABLE,
-    )
 
 
 # pylint: disable=too-few-public-methods, missing-class-docstring
