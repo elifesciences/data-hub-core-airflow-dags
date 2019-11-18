5055d220c58f6b45fc06657ba7bc687626b1376e
michael <m.owonibi@elifesciences.org>
2019-11-18 13:05:48 +0000

    #5183 ci pipeline end to end tests improved unit testing

--- run_test.sh
+++ run_test.sh
@@ -25,6 +25,7 @@ PYLINTHOME=/tmp/bigquery-views-pylint \
 echo "running flake8"
 flake8 flake8  tests/ data_pipeline/
 
+echo $1
 if  [ $1 == "with-end-to-end" ]; then
     echo "running end to end tests"
     pytest tests/end2end_test/ -p no:cacheprovider -s --disable-warnings
