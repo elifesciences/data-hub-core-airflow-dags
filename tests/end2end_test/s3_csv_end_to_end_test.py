ab9fee741ac04257c5ca4cc6e6abfa99cdd2f1a1
michael <m.owonibi@elifesciences.org>
2020-04-08 02:01:46 +0100

    web api data pipeline tests added

--- tests/end2end_test/s3_csv_end_to_end_test.py
+++ tests/end2end_test/s3_csv_end_to_end_test.py
@@ -1,79 +1,49 @@
 import os
 import logging
-import time
 
+from data_pipeline import get_yaml_file_as_dict
+from data_pipeline.s3_csv_data.s3_csv_config import (
+    S3BaseCsvConfig, MultiS3CsvConfig
+)
 from dags.s3_csv_import_controller import (
     DAG_ID,
     TARGET_DAG,
     S3_CSV_CONFIG_FILE_PATH_ENV_NAME,
-    get_yaml_file_as_dict,
 )
-
 from dags.s3_csv_import_pipeline import (
     DEFAULT_DEPLOYMENT_ENV_VALUE, DEPLOYMENT_ENV_ENV_NAME
 )
 from tests.end2end_test.end_to_end_test_helper import (
-    AirflowAPI, simple_query
+    AirflowAPI
 )
-from data_pipeline.s3_csv_data.s3_csv_config import (
-    S3BaseCsvConfig, MultiS3CsvConfig
-)
-from data_pipeline.utils.data_store.s3_data_service import delete_s3_object
+from tests.end2end_test import trigger_run_test_pipeline
 
 LOGGER = logging.getLogger(__name__)
-AIRFLOW_API = AirflowAPI()
 
 
-# pylint: disable=broad-except
 def test_dag_runs_data_imported():
-    (project_name,
-     dataset_name,
-     table_name,
-     state_file_bucket_name,
-     state_file_object_name
-     ) = get_project_dataset_table()
+    airflow_api = AirflowAPI()
+    (
+        project_name,
+        dataset_name,
+        table_name,
+        state_file_bucket_name,
+        state_file_object_name
+    ) = get_data_pipeline_cloud_resource()
 
-    try:
-        simple_query(
-            query=TestQueryTemplate.CLEAN_TABLE_QUERY,
-            project=project_name,
-            dataset=dataset_name,
-            table=table_name,
-        )
-    except Exception:
-        LOGGER.info("table not cleaned, maybe it does not exist")
-    try:
-        delete_s3_object(state_file_bucket_name,
-                         state_file_object_name
-                         )
-    except Exception:
-        LOGGER.info("s3 object not deleted, may not exist")
-
-    AIRFLOW_API.unpause_dag(TARGET_DAG)
-    execution_date = AIRFLOW_API.trigger_dag(dag_id=DAG_ID)
-    is_dag_running = True
-    while is_dag_running:
-        is_dag_running = AIRFLOW_API.is_dag_running(DAG_ID, execution_date)
-        if not is_dag_running:
-            time.sleep(15)
-            is_dag_running = AIRFLOW_API.is_triggered_dag_running(
-                TARGET_DAG
-            )
-        LOGGER.info("etl in progress")
-        time.sleep(5)
-    time.sleep(15)
-    assert not is_dag_running
-    assert AIRFLOW_API.get_dag_status(DAG_ID, execution_date) == "success"
-    query_response = simple_query(
-        query=TestQueryTemplate.READ_COUNT_TABLE_QUERY,
-        project=project_name,
-        dataset=dataset_name,
-        table=table_name,
+    trigger_run_test_pipeline(
+        airflow_api,
+        DAG_ID,
+        TARGET_DAG,
+        project_name,
+        dataset_name,
+        table_name,
+        state_file_bucket_name,
+        state_file_object_name
     )
-    assert query_response[0].get("count") > 0
 
 
-def get_project_dataset_table():
+def get_data_pipeline_cloud_resource():
     conf_file_path = os.getenv(
         S3_CSV_CONFIG_FILE_PATH_ENV_NAME
     )
@@ -97,13 +67,3 @@ def get_project_dataset_table():
         multi_s3_object_pattern_config.state_file_bucket_name,
         multi_s3_object_pattern_config.state_file_object_name
     )
-
-
-# pylint: disable=too-few-public-methods, missing-class-docstring
-class TestQueryTemplate:
-    CLEAN_TABLE_QUERY = """
-    Delete from `{project}.{dataset}.{table}` where true
-    """
-    READ_COUNT_TABLE_QUERY = """
-    Select Count(*) AS count from `{project}.{dataset}.{table}`
-    """
