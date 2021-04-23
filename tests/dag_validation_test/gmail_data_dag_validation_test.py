75d3db231f44416e048169f0b8f1dd374fde0175
HazalCiplak <hazalciplak@gmail.com>
2021-04-23 20:13:50 +0300

    fixed lint errors

--- tests/dag_validation_test/gmail_data_dag_validation_test.py
+++ tests/dag_validation_test/gmail_data_dag_validation_test.py
@@ -21,10 +21,9 @@ def test_dag_should_contain_named_tasks(dagbag):
         'gmail_thread_details_from_temp_thread_ids_etl',
         'gmail_thread_details_from_temp_history_details_etl',
         'delete_temp_table_labels',
-        'delete_temp_table_thread_ids', 
+        'delete_temp_table_thread_ids',
         'delete_temp_table_history_details',
         'load_from_temp_table_to_label_list',
         'load_from_temp_table_to_thread_ids_list'
     ]
     dag_should_contain_named_tasks(dagbag, DAG_ID, gm_task_list)
-
