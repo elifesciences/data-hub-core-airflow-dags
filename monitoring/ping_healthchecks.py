3634589201b26199a20d91b9562ba8107679f9ea
HazalCiplak <hazalciplak@gmail.com>
2021-07-21 17:15:43 +0100

    added health check env var

--- monitoring/ping_healthchecks.py
+++ monitoring/ping_healthchecks.py
@@ -1,20 +1,24 @@
+import os
 import logging
 import socket
 import requests
 
 LOGGER = logging.getLogger(__name__)
 
-# Schedule of healthchecks.io
-    # Period (Expected time between pings):	3 hours
-    # Grace Time (When a check is late, how long to wait until an alert is sent): 1 hour
-HEALTH_CHECKS_URL = "https://hc-ping.com/3549bc53-4bd9-4ef4-9c4a-22aa57c2fb5b"
-# as the url shoul not be public what can be the option for the make it private?
+HEALTH_CHECK_URL_ENV = "HEALTH_CHECK_URL"
+DEFAULT_HEALTH_CHECK_URL_ENV = "https://hc-ping.com/3549bc53-4bd9-4ef4-9c4a-22aa57c2fb5b"
+
+
+def get_env_var_or_use_default(env_var_name, default_value=None):
+    return os.getenv(env_var_name, default_value)
 
 
 def ping():
-    LOGGER.info('[healthcheck] pinging alive status...')
+    url = get_env_var_or_use_default(
+        HEALTH_CHECK_URL_ENV, DEFAULT_HEALTH_CHECK_URL_ENV)
+    LOGGER.info('[healthcheck] pinging url: %s', url)
     try:
-        requests.post(HEALTH_CHECKS_URL)
+        requests.post(url)
     except socket.error as err:
         LOGGER.info("Ping failed: %s", err)
 
