0fa5e31ce4d4dacc8870f3d6bbe5d335ef334ae5
Hazal <HazalCiplak@users.noreply.github.com>
2021-08-02 14:55:13 +0100

    Remove default slack url (#615)
    * removed dafault slack webhook url
    
    * added dependancy between tasks
    
    * added check to webhook url instead of deployment env

--- data_pipeline/monitoring/data_hub_pipeline_health_check.py
+++ data_pipeline/monitoring/data_hub_pipeline_health_check.py
@@ -11,10 +11,6 @@ DEPLOYMENT_ENV_ENV_NAME = "DEPLOYMENT_ENV"
 DEFAULT_DEPLOYMENT_ENV = "staging"
 
 DATA_HUB_MONITORING_SLACK_WEBHOOK_URL_ENV_NAME = "DATA_HUB_MONITORING_SLACK_WEBHOOK_URL"
-DEFAULT_DATA_HUB_MONITORING_SLACK_WEBHOOK_URL = (
-    "https://hooks.slack.com/services/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"
-)
-
 
 LOGGER = logging.getLogger(__name__)
 
@@ -77,11 +73,7 @@ def get_changed_status_df(
     ]
 
 
-def send_slack_message(message: str):
-    webhook_url = os.getenv(
-        DATA_HUB_MONITORING_SLACK_WEBHOOK_URL_ENV_NAME,
-        DEFAULT_DATA_HUB_MONITORING_SLACK_WEBHOOK_URL
-    )
+def send_slack_message(message: str, webhook_url: str):
     LOGGER.info('slack webhook url: %s', webhook_url)
     response = requests.post(webhook_url, json={
         'text': message
@@ -104,10 +96,11 @@ def get_formatted_changed_status_slack_message(  # pylint: disable=invalid-name
 
 
 def send_slack_notification(
-        changed_status_df: pd.DataFrame):
-    send_slack_message(get_formatted_changed_status_slack_message(
-        changed_status_df
-    ))
+        changed_status_df: pd.DataFrame, webhook_url: str):
+    send_slack_message(
+        get_formatted_changed_status_slack_message(changed_status_df),
+        webhook_url
+    )
 
 
 def run_data_hub_pipeline_health_check(
@@ -129,6 +122,9 @@ def run_data_hub_pipeline_health_check(
         object_name=object_name
     )
     status_df = read_big_query(query, project_id=project)
+    webhook_url = os.getenv(
+        DATA_HUB_MONITORING_SLACK_WEBHOOK_URL_ENV_NAME
+    )
     with pd.option_context("display.max_rows", 100, "display.max_columns", 10):
         LOGGER.info('status_df:\n%s', status_df)
         if previous_status_df is not None:
@@ -140,7 +136,8 @@ def run_data_hub_pipeline_health_check(
             changed_status_df = get_changed_status_df(merged_status_df)
             if len(changed_status_df) > 0:  # pylint: disable=len-as-condition
                 LOGGER.info('changed: %s', changed_status_df.to_dict(orient='rows'))
-                send_slack_notification(changed_status_df)
+                if webhook_url:
+                    send_slack_notification(changed_status_df, webhook_url)
             else:
                 LOGGER.info('no changes')
 
