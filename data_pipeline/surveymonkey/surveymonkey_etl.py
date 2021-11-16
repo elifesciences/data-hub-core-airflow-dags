ccd52c563e8ea530c1fa609b6e4902b94eca37fd
HazalCiplak <hazalciplak@gmail.com>
2021-11-16 15:46:29 +0000

    added imported_timestamp to survey list

--- data_pipeline/surveymonkey/surveymonkey_etl.py
+++ data_pipeline/surveymonkey/surveymonkey_etl.py
@@ -1,7 +1,12 @@
+import datetime
 import logging
 import requests
 
 LOGGER = logging.getLogger(__name__)
+
+
+def get_current_timestamp():
+    return datetime.datetime.utcnow().isoformat()
 
 
 def get_surveymonkey_api_headers(access_token: str) -> dict:
@@ -12,7 +17,10 @@ def get_surveymonkey_api_headers(access_token: str) -> dict:
 
 
 def get_survey_list(access_token: str) -> list:
-    headers = get_surveymonkey_api_headers(access_token)
+    headers=get_surveymonkey_api_headers(access_token)
     response = requests.get('https://api.surveymonkey.com/v3/surveys', headers=headers)
     response.raise_for_status()
-    return response.json()['data']
+    survey_list = response.json()["data"]
+    for survey in survey_list:
+        survey["imported_timestamp"] = get_current_timestamp()
+    return survey_list
