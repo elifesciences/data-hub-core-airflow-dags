60695932ec225ca09371bf2f01f496a3c5980441
HazalCiplak <hazalciplak@gmail.com>
2021-11-19 15:24:31 +0000

    added tests for new fields

--- tests/unit_test/surveymonkey/surveymonkey_etl_test.py
+++ tests/unit_test/surveymonkey/surveymonkey_etl_test.py
@@ -7,8 +7,8 @@ DEFAULT_SURVEY_RESPONSE_JSON = {
     "id": "DEFAULT_ID",
     "pages": [],
     "imported_timestamp": "",
-    "response_count": "",
-    "date_modified": ""
+    "response_count": 0,
+    "date_modified": "DEFAULT_DATE"
 }
 
 
@@ -20,17 +20,29 @@ class TestGetBqJsonForSurveyQuestionsResponseJson():
         )
         assert result["title"] == "TITLE"
 
-    def test_should_ignore_other_field(self):
-        result = get_bq_json_for_survey_questions_response_json(
-            {**DEFAULT_SURVEY_RESPONSE_JSON, "other": "OTHER"}
-        )
-        assert result.get("other") is None
-
     def test_should_extract_id_as_survey_id(self):
         result = get_bq_json_for_survey_questions_response_json(
             {**DEFAULT_SURVEY_RESPONSE_JSON, "id": "ID"}
         )
         assert result["survey_id"] == "ID"
+
+    def test_should_extract_response_count(self):
+        result = get_bq_json_for_survey_questions_response_json(
+            {**DEFAULT_SURVEY_RESPONSE_JSON, "response_count": 13}
+        )
+        assert result["response_count"] == 13
+    
+    def test_should_extract_date_modified(self):
+        result = get_bq_json_for_survey_questions_response_json(
+            {**DEFAULT_SURVEY_RESPONSE_JSON, "date_modified": "2021-11-19"}
+        )
+        assert result["date_modified"] == "2021-11-19"
+
+    def test_should_ignore_other_field(self):
+        result = get_bq_json_for_survey_questions_response_json(
+            {**DEFAULT_SURVEY_RESPONSE_JSON, "other": "OTHER"}
+        )
+        assert result.get("other") is None
 
     def test_shoud_extract_question(self):
         result = get_bq_json_for_survey_questions_response_json({
