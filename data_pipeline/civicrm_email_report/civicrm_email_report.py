a9675b529afa343dede49f4840cdb0f1adc36100
HazalCiplak <hazalciplak@gmail.com>
2022-02-03 11:45:12 +0000

    added getting email report

---
+++ data_pipeline/civicrm_email_report/civicrm_email_report.py
@@ -0,0 +1,39 @@
+import json
+import requests
+
+
+def get_mailing_id_dict(mail_id: int) -> str(dict):
+    return json.dumps({'mailing_id': mail_id})
+
+
+def get_connection_parameters(mail_id: int, api_key, site_key):
+    return {
+        "entity": "Mailing",
+        "action": "stats",
+        "json" : get_mailing_id_dict(mail_id),
+        "api_key": api_key,
+        "key": site_key
+    }
+
+
+def get_email_report(
+    url: str,
+    mail_id: int,
+    api_key: str,
+    site_key: str
+) -> dict:
+    response = requests.post(url=url, data=get_connection_parameters(mail_id, api_key, site_key))
+    response.raise_for_status()
+    dict_response = eval(response.text)
+    mail_id_str = str(mail_id)
+    return {
+        "mail_id": dict_response["id"],
+        "delivered": dict_response["values"][mail_id_str]["Delivered"],
+        "delivered_rate": dict_response["values"][mail_id_str]["delivered_rate"],
+        "bounces": dict_response["values"][mail_id_str]["Bounces"],
+        "unsubscribers": dict_response["values"][mail_id_str]["Unsubscribers"],
+        "unique_clicks": dict_response["values"][mail_id_str]["Unique Clicks"],
+        "opened": dict_response["values"][mail_id_str]["Opened"],
+        "opened_rate": dict_response["values"][mail_id_str]["opened_rate"],
+        "clickthrough_rate": dict_response["values"][mail_id_str]["clickthrough_rate"]
+    }
