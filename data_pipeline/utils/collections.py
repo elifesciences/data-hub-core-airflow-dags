e0bd95295952bbe0a41333cede73b0c85b4d8f84
Daniel Ecer <de-code@users.noreply.github.com>
2022-03-14 10:46:14 +0000

    linting: renamed to iterator

--- data_pipeline/utils/collections.py
+++ data_pipeline/utils/collections.py
@@ -5,9 +5,9 @@ from typing import Iterable, T
 def iter_batches_iterable(
         long_list: Iterable[T],
         batch_size: int) -> Iterable[Iterable[T]]:
-    it = iter(long_list)
+    iterator = iter(long_list)
     while True:
-        batch = list(islice(it, batch_size))
+        batch = list(islice(iterator, batch_size))
         if not batch:
             return
         yield batch
