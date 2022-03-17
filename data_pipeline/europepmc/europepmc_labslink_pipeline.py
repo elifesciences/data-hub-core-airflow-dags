6bf5554645992acc426b7c26a9f957c2517ac047
Daniel Ecer <de-code@users.noreply.github.com>
2022-03-17 12:00:10 +0000

    call fetch_article_dois_from_bigquery

--- data_pipeline/europepmc/europepmc_labslink_pipeline.py
+++ data_pipeline/europepmc/europepmc_labslink_pipeline.py
@@ -1,6 +1,8 @@
 import logging
+from typing import Sequence
 
 from data_pipeline.europepmc.europepmc_labslink_config import (
+    BigQuerySourceConfig,
     EuropePmcLabsLinkConfig
 )
 
@@ -8,7 +10,16 @@ from data_pipeline.europepmc.europepmc_labslink_config import (
 LOGGER = logging.getLogger(__name__)
 
 
+def fetch_article_dois_from_bigquery(
+    bigquery_source: BigQuerySourceConfig
+) -> Sequence[str]:
+    LOGGER.debug('bigquery_source: %r', bigquery_source)
+    return []
+
+
 def fetch_article_dois_from_bigquery_and_update_labslink_ftp(
     config: EuropePmcLabsLinkConfig
 ):
     LOGGER.debug('config: %r', config)
+    article_dois = fetch_article_dois_from_bigquery(config.source.bigquery)
+    LOGGER.debug('article_dois: %r', article_dois)
---
+++ tests/unit_test/europepmc/europepmc_labslink_pipeline_test.py
@@ -0,0 +1,63 @@
+from unittest.mock import MagicMock, patch
+
+import pytest
+
+
+from data_pipeline.europepmc.europepmc_labslink_config import (
+    BigQuerySourceConfig,
+    EuropePmcLabsLinkConfig,
+    EuropePmcLabsLinkSourceConfig,
+    EuropePmcLabsLinkTargetConfig,
+    FtpTargetConfig
+)
+import data_pipeline.europepmc.europepmc_labslink_pipeline as europepmc_labslink_pipeline_module
+from data_pipeline.europepmc.europepmc_labslink_pipeline import (
+    fetch_article_dois_from_bigquery_and_update_labslink_ftp
+)
+
+
+BIGQUERY_SOURCE_CONFIG_1 = BigQuerySourceConfig(
+    sql_query='query1'
+)
+
+SOURCE_CONFIG_1 = EuropePmcLabsLinkSourceConfig(
+    bigquery=BIGQUERY_SOURCE_CONFIG_1
+)
+
+FTP_TARGET_CONFIG_1 = FtpTargetConfig(
+    host='host1:234',
+    username='user1',
+    password='password1',
+    directory_name='dir1'
+)
+
+TARGET_CONFIG_1 = EuropePmcLabsLinkTargetConfig(
+    ftp=FTP_TARGET_CONFIG_1
+)
+
+CONFIG_1 = EuropePmcLabsLinkConfig(
+    source=SOURCE_CONFIG_1,
+    target=TARGET_CONFIG_1
+)
+
+
+@pytest.fixture(name='fetch_article_dois_from_bigquery_mock')
+def _fetch_article_dois_from_bigquery_mock() -> MagicMock:
+    with patch.object(
+        europepmc_labslink_pipeline_module,
+        'fetch_article_dois_from_bigquery'
+    ) as mock:
+        yield mock
+
+
+class TestFetchArticleDoisFromBigQueryAndUpdateLabsLinkFtp:
+    def test_should_call_fetch_article_dois_from_bigquery(
+        self,
+        fetch_article_dois_from_bigquery_mock: MagicMock
+    ):
+        fetch_article_dois_from_bigquery_and_update_labslink_ftp(
+            CONFIG_1
+        )
+        fetch_article_dois_from_bigquery_mock.assert_called_with(
+            BIGQUERY_SOURCE_CONFIG_1
+        )
