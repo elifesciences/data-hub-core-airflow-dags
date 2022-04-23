c3c74c1ec07872206f8615a79f3039951ea5a89a
Daniel Ecer <de-code@users.noreply.github.com>
2022-04-23 11:28:14 +0100

    read target config

--- data_pipeline/semantic_scholar/semantic_scholar_config.py
+++ data_pipeline/semantic_scholar/semantic_scholar_config.py
@@ -1,15 +1,21 @@
 from typing import NamedTuple
 
+from data_pipeline.utils.pipeline_config import BigQueryTargetConfig
+
 
 DEFAULT_BATCH_SIZE = 1000
 
 
 class SemanticScholarConfig(NamedTuple):
+    target: BigQueryTargetConfig
     batch_size: int = DEFAULT_BATCH_SIZE
 
     @staticmethod
     def _from_item_dict(item_config_dict: dict) -> 'SemanticScholarConfig':
         return SemanticScholarConfig(
+            target=BigQueryTargetConfig.from_dict(
+                item_config_dict['target']
+            ),
             batch_size=item_config_dict.get('batchSize') or DEFAULT_BATCH_SIZE
         )
 
--- tests/unit_test/semantic_scholar/semantic_scholar_config_test.py
+++ tests/unit_test/semantic_scholar/semantic_scholar_config_test.py
@@ -4,12 +4,28 @@ from data_pipeline.semantic_scholar.semantic_scholar_config import (
 )
 
 
+PROJECT_NAME_1 = 'project1'
+DATASET_NAME_1 = 'dataset1'
+TABLE_NAME_1 = 'table1'
+
+
+TARGET_1 = {
+    'projectName': PROJECT_NAME_1,
+    'datasetName': DATASET_NAME_1,
+    'tableName': TABLE_NAME_1
+}
+
+
 ITEM_CONFIG_DICT_1 = {
+    'target': TARGET_1
 }
 
 
 def get_config_for_item_config_dict(item_dict: dict) -> dict:
     return {'semanticScholar': [item_dict]}
+
+
+CONFIG_DICT_1 = get_config_for_item_config_dict(ITEM_CONFIG_DICT_1)
 
 
 class TestSemanticScholarConfig:
@@ -26,3 +42,9 @@ class TestSemanticScholarConfig:
             ITEM_CONFIG_DICT_1
         ))
         assert config.batch_size == DEFAULT_BATCH_SIZE
+
+    def test_should_read_target_project_dataset_and_table_name(self):
+        config = SemanticScholarConfig.from_dict(CONFIG_DICT_1)
+        assert config.target.project_name == PROJECT_NAME_1
+        assert config.target.dataset_name == DATASET_NAME_1
+        assert config.target.table_name == TABLE_NAME_1
