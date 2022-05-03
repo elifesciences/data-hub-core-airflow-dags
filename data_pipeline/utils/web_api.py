3a60d73006e2d3c6402ab3068809d1d210eff3d5
Daniel Ecer <de-code@users.noreply.github.com>
2022-05-03 14:36:21 +0100

    added type hints to requests_retry_session

--- data_pipeline/utils/web_api.py
+++ data_pipeline/utils/web_api.py
@@ -1,3 +1,4 @@
+from typing import Iterable, Optional
 import requests
 from requests.adapters import HTTPAdapter
 
@@ -5,11 +6,11 @@ from urllib3.util.retry import Retry
 
 
 def requests_retry_session(
-    retries=10,
-    backoff_factor=0.3,
-    status_forcelist=(500, 502, 504),
-    session=None,
-):
+    retries: int = 10,
+    backoff_factor: float = 0.3,
+    status_forcelist: Iterable[int] = (500, 502, 504),
+    session: Optional[requests.Session] = None,
+) -> requests.Session:
     session = session or requests.Session()
     retry = Retry(
         total=retries,
