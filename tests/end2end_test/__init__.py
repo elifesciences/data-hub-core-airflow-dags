4f3635f6e3db31febf3f34adc006757434a7a426
HazalCiplak <hazalciplak@gmail.com>
2022-05-30 17:47:51 +0100

    fixed the handling of 'queued' status for main dag test

--- tests/end2end_test/__init__.py
+++ tests/end2end_test/__init__.py
@@ -72,10 +72,9 @@ def enable_and_trigger_dag_and_wait_for_success(
     execution_date = airflow_api.trigger_dag(
         dag_id=dag_id, conf=dag_trigger_conf
     )
-    is_dag_running = wait_till_all_dag_run_ends(
+    wait_till_all_dag_run_ends(
         airflow_api, execution_date, dag_id, target_dag,
     )
-    assert not is_dag_running
     assert airflow_api.get_dag_status(dag_id, execution_date) == "success"
 
 
@@ -114,22 +113,37 @@ def trigger_run_test_pipeline(
     assert loaded_table_row_count > 0
 
 
+def wait_untill_dag_run_is_successful(
+    airflow_api,
+    dag_id: str,
+    execution_date: str
+) -> None:
+    dag_status: str
+    while True:
+        dag_status = airflow_api.get_dag_status(dag_id, execution_date)
+        if dag_status not in {"running", "queued"}:
+            break
+        time.sleep(5)
+        LOGGER.info("etl in progress (status: %r)", dag_status)
+    assert dag_status == "success"
+
+
 def wait_till_all_dag_run_ends(
         airflow_api, execution_date,
         dag_id, triggered_dag_id=None
 ):
-    is_dag_running = True
-    while is_dag_running:
-        is_dag_running = airflow_api.is_dag_running(dag_id, execution_date)
-        if not is_dag_running and triggered_dag_id:
-            time.sleep(15)
-            is_dag_running = airflow_api.is_triggered_dag_running(
+    wait_untill_dag_run_is_successful(
+        airflow_api=airflow_api,
+        dag_id=dag_id,
+        execution_date=execution_date
+    )
+    if triggered_dag_id:
+        while airflow_api.is_triggered_dag_running(triggered_dag_id):
+            LOGGER.info(
+                "waiting for the DAG triggered by the controller. Dag id is %s",
                 triggered_dag_id
             )
-        LOGGER.info("etl in progress")
-        time.sleep(5)
-    time.sleep(15)
-    return is_dag_running
+            time.sleep(5)
 
 
 class TestQueryTemplate:
