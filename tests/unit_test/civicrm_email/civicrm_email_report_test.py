b53e60219206a8b1592718941f8cf80e92399252
Hazal <HazalCiplak@users.noreply.github.com>
2022-06-01 17:50:07 +0100

    Add Unique Opens to civiCRM email summary report (#1001)
    * added new columns in civicrm email report
    
    * corrected column name
    
    * chnaged civicrm unittest input data
    
    * added new test for new columns
    
    * linting

--- tests/unit_test/civicrm_email/civicrm_email_report_test.py
+++ tests/unit_test/civicrm_email/civicrm_email_report_test.py
@@ -13,17 +13,30 @@ CLICKTHROUGH_RATE_VALUE = "30.30%"
 OPENED_RATE_VALUE = "180.5%"
 DELIVERED_RATE_VALUE = "99%"
 
+DISTINCT_UNIQUE_CLICKS_VALUE = "10"
+DISTINCT_OPENED_VALUE = "50"
+
 DEFAULT_CIVICRM_EMAIL_REPORT_RESPONSE = {
-    "values": {
-        str(MAIL_ID): {
-            "Delivered": DELIVERED_VALUE,
-            "Bounces": BOUNCES_VALUE,
-            "Unsubscribers": UNSUBSCRIBERS_VALUE,
-            "Unique Clicks": UNIQUE_CLICKS_VALUE,
-            "Opened": OPENED_VALUE,
-            "clickthrough_rate": CLICKTHROUGH_RATE_VALUE,
-            "opened_rate": OPENED_RATE_VALUE,
-            "delivered_rate": DELIVERED_RATE_VALUE
+    "report_with_non_distinct_values": {
+        "values": {
+            str(MAIL_ID): {
+                "Delivered": DELIVERED_VALUE,
+                "Bounces": BOUNCES_VALUE,
+                "Unsubscribers": UNSUBSCRIBERS_VALUE,
+                "Unique Clicks": UNIQUE_CLICKS_VALUE,
+                "Opened": OPENED_VALUE,
+                "clickthrough_rate": CLICKTHROUGH_RATE_VALUE,
+                "opened_rate": OPENED_RATE_VALUE,
+                "delivered_rate": DELIVERED_RATE_VALUE
+            }
+        }
+    },
+    "report_with_distinct_values": {
+        "values": {
+            str(MAIL_ID): {
+                "Unique Clicks": DISTINCT_UNIQUE_CLICKS_VALUE,
+                "Opened": DISTINCT_OPENED_VALUE
+            }
         }
     }
 }
@@ -43,7 +56,7 @@ class TestTransformEmailReport():
             DEFAULT_CIVICRM_EMAIL_REPORT_RESPONSE,
             MAIL_ID
         )
-        assert result["delivered"] == int(DELIVERED_VALUE)
+        assert result["successful_deliveries"] == int(DELIVERED_VALUE)
 
     def test_should_extract_bounces_as_integer(self):
         result = transform_email_report(
@@ -52,26 +65,40 @@ class TestTransformEmailReport():
         )
         assert result["bounces"] == int(BOUNCES_VALUE)
 
-    def test_should_extract_unsubscribers_as_integer(self):
+    def test_should_extract_unsubscribe_requests_as_integer(self):
         result = transform_email_report(
             DEFAULT_CIVICRM_EMAIL_REPORT_RESPONSE,
             MAIL_ID
         )
-        assert result["unsubscribers"] == int(UNSUBSCRIBERS_VALUE)
+        assert result["unsubscribe_requests"] == int(UNSUBSCRIBERS_VALUE)
+
+    def test_should_extract_total_clicks_as_integer(self):
+        result = transform_email_report(
+            DEFAULT_CIVICRM_EMAIL_REPORT_RESPONSE,
+            MAIL_ID
+        )
+        assert result["total_clicks"] == int(UNIQUE_CLICKS_VALUE)
 
     def test_should_extract_unique_clicks_as_integer(self):
         result = transform_email_report(
             DEFAULT_CIVICRM_EMAIL_REPORT_RESPONSE,
             MAIL_ID
         )
-        assert result["unique_clicks"] == int(UNIQUE_CLICKS_VALUE)
+        assert result["unique_clicks"] == int(DISTINCT_UNIQUE_CLICKS_VALUE)
 
-    def test_should_extract_opened_as_integer(self):
+    def test_should_extract_total_opens_as_integer(self):
         result = transform_email_report(
             DEFAULT_CIVICRM_EMAIL_REPORT_RESPONSE,
             MAIL_ID
         )
-        assert result["opened"] == int(OPENED_VALUE)
+        assert result["total_opens"] == int(OPENED_VALUE)
+
+    def test_should_extract_unique_opens_as_integer(self):
+        result = transform_email_report(
+            DEFAULT_CIVICRM_EMAIL_REPORT_RESPONSE,
+            MAIL_ID
+        )
+        assert result["unique_opens"] == int(DISTINCT_OPENED_VALUE)
 
     def test_should_extract_delivered_rate_as_float(self):
         result = transform_email_report(
