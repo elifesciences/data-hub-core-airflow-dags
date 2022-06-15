919c346e0e49815d558d0d9d53ea1739751a663f
HazalCiplak <hazalciplak@gmail.com>
2022-06-15 17:20:08 +0300

    added func iter_xml_file_url_from_git_directory

--- data_pipeline/related_articles/related_articles_pipeline.py
+++ data_pipeline/related_articles/related_articles_pipeline.py
@@ -1,10 +1,12 @@
+from typing import Iterable
 import requests
 
-from data_pipeline.related_articles.related_artcicles_config import (
+
+from data_pipeline.related_articles.related_articles_config import (
     RelatedArticlesSourceConfig
 )
 
-def get_url_of_directory_from_repo_tree(
+def get_url_of_xml_file_directory_from_repo(
     source_config: RelatedArticlesSourceConfig,
 ) -> str:
     response = requests.get(
@@ -14,6 +16,20 @@ def get_url_of_directory_from_repo_tree(
     response.raise_for_status()
     reponse_json = response.json()
     for folder in reponse_json['tree']:
-        if folder['path']=='articles':
+        if folder['path']==source_config.directory_name:
             return folder['url']
 
+
+def iter_xml_file_url_from_git_directory(
+    source_config: RelatedArticlesSourceConfig
+) -> Iterable[str]:
+    article_xml_directory_url = get_url_of_xml_file_directory_from_repo(source_config=source_config)
+    response = requests.get(
+        url=article_xml_directory_url,
+        headers=source_config.headers
+    )
+    response.raise_for_status()
+    reponse_json = response.json()
+    for article_xml_url in reponse_json['tree']:
+        if article_xml_url['size'] > 0:
+            yield article_xml_url['url']
