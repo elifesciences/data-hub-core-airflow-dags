56456be73b039caed1cc352ae427759e4899a842
HazalCiplak <hazalciplak@gmail.com>
2022-06-15 15:24:59 +0300

    added RelatedArticlesConfig

--- data_pipeline/related_articles/related_artcicles_config.py
+++ data_pipeline/related_articles/related_artcicles_config.py
@@ -1,5 +1,6 @@
 from typing import NamedTuple
 
+from data_pipeline.utils.pipeline_config import BigQueryTargetConfig
 
 class RelatedArticlesSourceConfig(NamedTuple):
     git_repo_url: str
@@ -10,4 +11,19 @@ class RelatedArticlesSourceConfig(NamedTuple):
         return RelatedArticlesSourceConfig(
             git_repo_url=source_config_dict['gitRepoUrl'],
             headers=source_config_dict['headers']
+        )
+
+class RelatedArticlesConfig(NamedTuple):
+    source_config: RelatedArticlesSourceConfig
+    target_config: BigQueryTargetConfig
+
+    @staticmethod
+    def _from_item_dict(item_config_dict) -> 'RelatedArticlesConfig':
+        return RelatedArticlesConfig(
+            source_config=RelatedArticlesSourceConfig.from_dict(
+                item_config_dict['source']
+            ),
+            target=BigQueryTargetConfig.from_dict(
+                item_config_dict['target']
+            )
         )
