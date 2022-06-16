3be8fe660ec4c5662dbae0ac7b4e5c83c887f8e6
HazalCiplak <hazalciplak@gmail.com>
2022-06-16 15:48:36 +0300

    added test for elife_article_pipeline

---
+++ data_pipeline/elife_article_xml/elife_article_xml_pipeline.py
@@ -0,0 +1,50 @@
+import base64
+from typing import Iterable, Any
+import requests
+
+
+from data_pipeline.elife_article_xml.elife_article_xml_config import (
+    RelatedArticlesSourceConfig
+)
+
+
+def get_json_response_from_url(url: str) -> Any:
+    response = requests.get(url=url)
+    response.raise_for_status()
+    return response.json()
+
+
+def get_url_of_xml_file_directory_from_repo(
+    source_config: RelatedArticlesSourceConfig,
+) -> str:
+    response_json = get_json_response_from_url(url=source_config.git_repo_url)
+    for folder in response_json['tree']:
+        if folder['path']==source_config.directory_name:
+            return folder['url']
+
+
+def iter_xml_file_url_from_git_directory(
+    source_config: RelatedArticlesSourceConfig
+) -> Iterable[str]:
+    response_json = get_json_response_from_url(
+        url=get_url_of_xml_file_directory_from_repo(source_config=source_config)
+    )
+    for article_xml_url in response_json['tree']:
+        if article_xml_url['size'] > 0:
+            yield article_xml_url['url']
+
+
+def iter_decoded_xml_file_content(
+    article_xml_url_list: Iterable[str]
+) -> Iterable[str]:
+    # dont forget to update the list
+    for article_xml_url in article_xml_url_list:
+        response_json = get_json_response_from_url(url=article_xml_url)
+        yield base64.b64decode(response_json['content']).decode('utf-8')
+
+
+def fetch_related_article_from_elife_article_xml_repo_and_load_into_bigquery(
+    source_config: RelatedArticlesSourceConfig
+):  
+    article_xml_url_list = iter_xml_file_url_from_git_directory(source_config=source_config)
+    iter_decoded_xml_file_content(article_xml_url_list)
