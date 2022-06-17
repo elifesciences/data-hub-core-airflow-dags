6d634d29d3cb1d8cde2db6a95e80e532711e07b7
HazalCiplak <hazalciplak@gmail.com>
2022-06-17 10:02:47 +0300

    added test_should_return_decoded_file_content

--- tests/unit_test/elife_article_xml/elife_article_xml_pipeline_test.py
+++ tests/unit_test/elife_article_xml/elife_article_xml_pipeline_test.py
@@ -27,12 +27,6 @@ def get_url_of_xml_file_directory_from_repo_mock():
         yield mock
 
 
-# CONTENT_ENCODED = 'TWVyaGFiYSBEdW55YSEgSGVsbG8gV29ybGQh'
-# CONTENT_DECODED = 'Merhaba Dunya! Hello World!'
-
-# XML_FILE_JSON = {
-#     'content': CONTENT_ENCODED
-# }
 MATCHING_DIRECTORY_NAME = 'matching_directory'
 OTHER_DIRECTORY_NAME = 'other_directory'
 
@@ -40,16 +34,16 @@ MATCHING_URL = 'matching_url'
 OTHER_URL = 'other_url'
 
 GITHUB_TREE_REPONSE_1 = {
-  'tree': [
-    {
-      'path': MATCHING_DIRECTORY_NAME,
-      'url': MATCHING_URL
-    },
-    {
-      'path': OTHER_DIRECTORY_NAME,
-      'url': OTHER_URL
-    }
-  ]
+    'tree': [
+        {
+            'path': MATCHING_DIRECTORY_NAME,
+            'url': MATCHING_URL
+        },
+        {
+            'path': OTHER_DIRECTORY_NAME,
+            'url': OTHER_URL
+        }
+    ]
 }
 
 EMPTY_ARTICLE_URL = 'empty_article_url'
@@ -57,20 +51,20 @@ NOT_EMPTY_ARTICLE_URL_1 = 'not_empty_article_url_1'
 NOT_EMPTY_ARTICLE_URL_2 = 'not_empty_article_url_2'
 
 GITHUB_TREE_REPONSE_2 = {
-  'tree': [
-    {
-      'size': 0,
-      'url': EMPTY_ARTICLE_URL
-    },
-    {
-      'size': 10,
-      'url': NOT_EMPTY_ARTICLE_URL_1
-    },
-    {
-      'size': 15,
-      'url': NOT_EMPTY_ARTICLE_URL_2
-    }
-  ]
+    'tree': [
+        {
+            'size': 0,
+            'url': EMPTY_ARTICLE_URL
+        },
+        {
+            'size': 10,
+            'url': NOT_EMPTY_ARTICLE_URL_1
+        },
+        { 
+            'size': 15,
+            'url': NOT_EMPTY_ARTICLE_URL_2
+        }
+    ]
 }
 
 GIT_REPO_URL = 'git_repo_url_1'
@@ -79,6 +73,17 @@ SOURCE_CONFIG_1 = RelatedArticlesSourceConfig(
     git_repo_url=GIT_REPO_URL,
     directory_name=MATCHING_DIRECTORY_NAME
 )
+
+
+XML_FILE_CONTENT_ENCODED = 'TWVyaGFiYSBEdW55YSEgSGVsbG8gV29ybGQh'
+XML_FILE_CONTENT_DECODED = 'Merhaba Dunya! Hello World!'
+
+XML_FILE_JSON = {
+    'content': XML_FILE_CONTENT_ENCODED,
+    'encoding': 'base64'
+}
+
+XML_FILE_URL_LIST = ['xml_file_url_1']
 
 
 class TestGetUrlOfXmlFileDirectoryFromRepo:
@@ -101,3 +106,13 @@ class TestIterXmlFileUrlFromGitDirectory:
         get_url_of_xml_file_directory_from_repo_mock.return_value = GITHUB_TREE_REPONSE_2
         actual_return_value = list(iter_xml_file_url_from_git_directory(SOURCE_CONFIG_1))
         assert actual_return_value == [NOT_EMPTY_ARTICLE_URL_1, NOT_EMPTY_ARTICLE_URL_2]
+
+
+class TestIterDecodedXmlFileContent:
+    def test_should_return_decoded_file_content(
+        self,
+        get_json_response_from_url_mock: MagicMock
+    ):
+        get_json_response_from_url_mock.return_value = XML_FILE_JSON
+        actual_return_value = list(iter_decoded_xml_file_content(XML_FILE_URL_LIST))
+        assert actual_return_value == [XML_FILE_CONTENT_DECODED]
