df02432166bc2fe063e3a9b657141a575242cf0d
HazalCiplak <hazalciplak@gmail.com>
2022-06-21 17:56:37 +0300

    added TestGetArticleJsonDataFromXmlStringContent

--- tests/unit_test/elife_article_xml/elife_article_xml_pipeline_test.py
+++ tests/unit_test/elife_article_xml/elife_article_xml_pipeline_test.py
@@ -4,19 +4,33 @@ import pytest
 from data_pipeline.elife_article_xml import (
     elife_article_xml_pipeline as elife_article_xml_pipeline_module
 )
-from data_pipeline.elife_article_xml.elife_article_xml_config import ElifeArticleXmlSourceConfig
+from data_pipeline.elife_article_xml.elife_article_xml_config import (
+    ElifeArticleXmlConfig,
+    ElifeArticleXmlSourceConfig
+)
 
 from data_pipeline.elife_article_xml.elife_article_xml_pipeline import (
     get_json_response_from_url,
     get_url_of_xml_file_directory_from_repo,
     iter_xml_file_url_from_git_directory,
-    iter_decoded_xml_file_content
+    iter_decoded_xml_file_content,
+    get_article_json_data_from_xml_string_content
 )
+from data_pipeline.utils.pipeline_config import BigQueryTargetConfig
 
 
 @pytest.fixture(name='requests_mock', autouse=True)
 def _requests_mock():
     with patch.object(elife_article_xml_pipeline_module, 'requests') as mock:
+        yield mock
+
+
+@pytest.fixture(name='iter_xml_file_url_from_git_directory_mock', autouse=True)
+def _iter_xml_file_url_from_git_directory_mock():
+    with patch.object(
+        elife_article_xml_pipeline_module,
+        'iter_xml_file_url_from_git_directory'
+    ) as mock:
         yield mock
 
 
@@ -26,11 +40,32 @@ def _get_json_response_from_url_mock():
         yield mock
 
 
+@pytest.fixture(name='iter_decoded_xml_file_content_mock')
+def _iter_decoded_xml_file_content_mock():
+    with patch.object(elife_article_xml_pipeline_module, 'iter_decoded_xml_file_content') as mock:
+        yield mock
+
+
+@pytest.fixture(name='get_single_column_value_list_from_bq_query_mock')
+def _get_single_column_value_list_from_bq_query_mock():
+    with patch.object(
+        elife_article_xml_pipeline_module,
+        'get_single_column_value_list_from_bq_query'
+    ) as mock:
+        yield mock
+
+
 @pytest.fixture(name='get_url_of_xml_file_directory_from_repo_mock')
 def _get_url_of_xml_file_directory_from_repo_mock():
     with patch.object(
       elife_article_xml_pipeline_module, 'get_url_of_xml_file_directory_from_repo'
     ) as mock:
+        yield mock
+
+
+@pytest.fixture(name='parse_xml_and_return_it_as_dict_mock')
+def _parse_xml_and_return_it_as_dict_mock():
+    with patch.object(elife_article_xml_pipeline_module, 'parse_xml_and_return_it_as_dict') as mock:
         yield mock
 
 
@@ -88,6 +123,17 @@ SOURCE_CONFIG_1 = ElifeArticleXmlSourceConfig(
     directory_name=MATCHING_DIRECTORY_NAME
 )
 
+TARGET_CONFIG_1 = BigQueryTargetConfig(
+    project_name='project1',
+    dataset_name='dataset1',
+    table_name='table1'
+)
+
+CONFIG_1 = ElifeArticleXmlConfig(
+    source=SOURCE_CONFIG_1,
+    target=TARGET_CONFIG_1
+)
+
 
 XML_FILE_CONTENT_ENCODED = 'TWVyaGFiYSBEdW55YSEgSGVsbG8gV29ybGQh'
 XML_FILE_CONTENT_DECODED = 'Merhaba Dunya! Hello World!'
@@ -99,6 +145,45 @@ XML_FILE_JSON = {
 
 XML_FILE_URL_LIST = ['xml_file_url_1']
 
+ARTICLE_TYPE_1 = 'article_type_1'
+ARTICLE_ID_1 = 'article_id_1'
+ARTICLE_DOI_1 = 'article_doi_1'
+
+RELATED_ARTICLE_TYPE_1 = 'related_article_type_1'
+RELATED_ARTICLE_DOI_1 = 'related_article_doi_1'
+
+RELATED_ARTICLE_LIST = [
+    {
+        'related-article-type': RELATED_ARTICLE_TYPE_1,
+        '{http://www.w3.org/1999/xlink}href': RELATED_ARTICLE_DOI_1
+    }
+]
+
+XML_FILE_CONTENT_DICT_WITH_RELATED_ARTICLE_SECTION = {
+  'article': {
+    'article-type': ARTICLE_TYPE_1,
+    'front': [
+      {
+        'article-meta': [
+          {
+            'article-id': [
+              {
+                'pub-id-type': 'publisher-id',
+                '_text': ARTICLE_ID_1
+              },
+              {
+                'pub-id-type': 'doi',
+                '_text': ARTICLE_DOI_1
+              }
+            ],
+            'related-article': RELATED_ARTICLE_LIST
+          }
+        ]
+      }
+    ]
+  }
+}
+
 
 class TestGetJsonResponseFromUrl:
     def test_should_pass_url_to_request_get_function(
@@ -107,7 +192,6 @@ class TestGetJsonResponseFromUrl:
     ):
         get_json_response_from_url(url=GIT_REPO_URL)
         requests_mock.get.assert_called_with(url=GIT_REPO_URL)
-
 
 
 class TestGetUrlOfXmlFileDirectoryFromRepo:
@@ -142,3 +226,38 @@ class TestIterDecodedXmlFileContent:
         get_json_response_from_url_mock.return_value = XML_FILE_JSON
         actual_return_value = list(iter_decoded_xml_file_content(XML_FILE_URL_LIST))
         assert actual_return_value == [XML_FILE_CONTENT_DECODED]
+
+
+class TestGetArticleJsonDataFromXmlStringContent:
+    def test_should_return_empty_dict_if_the_xml_with_empty_root(self):
+        xml_string = '<article></article>'
+        return_value = get_article_json_data_from_xml_string_content(xml_string)
+        assert return_value == {}
+
+    def test_should_return_empty_dict_if_the_xml_has_empty_article_meta(self):
+        xml_string = '<article><front><article-meta></article-meta></front></article>'
+        return_value = get_article_json_data_from_xml_string_content(xml_string)
+        assert return_value == {}
+
+    def test_should_return_dict_with_related_article_if_it_exists(self):
+        xml_string = '''
+            <article><front><article-meta><related-article ext-link-type="doi" id="ra1" related-article-type="related_article_type_1" href="related_article_doi_1"/></article-meta></front></article>
+        '''
+        return_value = get_article_json_data_from_xml_string_content(xml_string)
+        assert return_value == {
+            'article':{
+                'front': [{
+                    'article_meta': [{
+                        'related_article':[{
+                            'ext_link_type': 'doi',
+                            'id': 'ra1',
+                            'related_article_type': 'related_article_type_1',
+                            'href': 'related_article_doi_1'
+                        }]
+                    }]
+                }]
+            }
+        }
+
+    def test_should_not_return_other_elements(self):
+        pass
