5d882010200292020c4dd1d03cb2839b1f11eaf9
Hazal <HazalCiplak@users.noreply.github.com>
2022-08-05 17:24:12 +0100

    added unit test for twitter ads pipeline (#1045)

---
+++ tests/unit_test/twitter_ads_api/twitter_ads_api_pipeline_test.py
@@ -0,0 +1,84 @@
+from unittest.mock import patch, MagicMock
+import pytest
+
+from data_pipeline.twitter_ads_api import (
+    twitter_ads_api_pipeline as twitter_ads_api_pipeline_module
+)
+
+from data_pipeline.twitter_ads_api.twitter_ads_api_pipeline import (
+    get_bq_compatible_json_response_from_resource
+)
+
+from data_pipeline.utils.pipeline_config import (
+    BigQueryTargetConfig
+)
+
+from data_pipeline.twitter_ads_api.twitter_ads_api_config import (
+    TwitterAdsApiSourceConfig
+)
+
+
+@pytest.fixture(name='get_client_from_twitter_ads_api_mock')
+def _get_client_from_twitter_ads_api_mock():
+    with patch.object(twitter_ads_api_pipeline_module, 'get_client_from_twitter_ads_api') as mock:
+        yield mock
+
+
+@pytest.fixture(name='remove_key_with_null_value_mock')
+def _remove_key_with_null_value_mock():
+    with patch.object(twitter_ads_api_pipeline_module, 'remove_key_with_null_value') as mock:
+        yield mock
+
+
+@pytest.fixture(name='request_mock', autouse=True)
+def _request_mock():
+    with patch.object(twitter_ads_api_pipeline_module, 'Request') as mock:
+        yield mock
+
+
+RESOURCE = 'resource_1'
+SECRETS = {'key1': 'value1', 'key2': 'value2'}
+
+SOURCE_CONFIG_1 = TwitterAdsApiSourceConfig(
+    resource=RESOURCE,
+    secrets=SECRETS
+)
+
+TARGET_CONFIG_1 = BigQueryTargetConfig(
+    project_name='project1',
+    dataset_name='dataset1',
+    table_name='table1'
+)
+
+RESPONSE_JSON_1 = {
+    'response_key_1': 'response_value_1',
+    'response_key_2': 'response_value_2'
+}
+
+
+class TestGetBqCompatibleJsonResponseRromResourceWithProvenance:
+    def test_should_pass_resource_and_params_to_request(
+        self,
+        request_mock: MagicMock,
+        get_client_from_twitter_ads_api_mock: MagicMock
+    ):
+        get_bq_compatible_json_response_from_resource(
+            SOURCE_CONFIG_1
+        )
+        request_mock.assert_called_with(
+            client=get_client_from_twitter_ads_api_mock(SOURCE_CONFIG_1),
+            method="GET",
+            resource=RESOURCE
+        )
+
+    def test_should_return_response_json(
+        self,
+        get_client_from_twitter_ads_api_mock: MagicMock,
+        remove_key_with_null_value_mock: MagicMock
+    ):
+        get_client_from_twitter_ads_api_mock.return_value = 'client'
+        remove_key_with_null_value_mock.return_value = RESPONSE_JSON_1
+        actual_response_json = get_bq_compatible_json_response_from_resource(
+            SOURCE_CONFIG_1
+        )
+        assert actual_response_json == RESPONSE_JSON_1
