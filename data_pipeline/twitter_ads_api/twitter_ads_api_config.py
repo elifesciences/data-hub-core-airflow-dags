60700b81ee5f120ba268f7a99d273e35b1349771
HazalCiplak <hazalciplak@gmail.com>
2022-08-09 12:58:23 +0100

    reflected changes on yaml to config

--- data_pipeline/twitter_ads_api/twitter_ads_api_config.py
+++ data_pipeline/twitter_ads_api/twitter_ads_api_config.py
@@ -1,20 +1,26 @@
 
-from typing import Mapping, NamedTuple
+from typing import Iterable, NamedTuple
 
-from data_pipeline.utils.pipeline_config import BigQueryTargetConfig, MappingConfig
+from data_pipeline.utils.pipeline_config import (
+    BigQuerySourceConfig,
+    BigQueryTargetConfig,
+    MappingConfig
+)
 
 
 class TwitterAdsApiSourceConfig(NamedTuple):
     resource: str
-    params: Mapping[str, str] = {}
     secrets: MappingConfig = MappingConfig.from_dict({})
+    bigquery: BigQuerySourceConfig = {}
+    param_names: Iterable[str] = []
 
     @staticmethod
     def from_dict(source_config_dict: dict) -> 'TwitterAdsApiSourceConfig':
         return TwitterAdsApiSourceConfig(
             resource=source_config_dict['resource'],
-            params=source_config_dict.get('params', {}),
-            secrets=MappingConfig.from_dict(source_config_dict['secrets'])
+            secrets=MappingConfig.from_dict(source_config_dict['secrets']),
+            param_names=source_config_dict.get('paramNames', []),
+            bigquery=BigQuerySourceConfig.from_dict(source_config_dict.get('bigQuery', {}))
         )
 
 
--- tests/unit_test/twitter_ads_api/twitter_ads_api_config_test.py
+++ tests/unit_test/twitter_ads_api/twitter_ads_api_config_test.py
@@ -5,14 +5,23 @@ from data_pipeline.twitter_ads_api.twitter_ads_api_config import (
 RESOURCE = 'resource_1'
 SECRETS = {'key1': 'value1', 'key2': 'value2'}
 
-SOURCE_CONFIG = {
-    'resource': RESOURCE,
-    'secrets': SECRETS
-}
+SQL_QUERY = 'query 1'
 
 PROJECT_NAME = 'project_1'
 DATASET_NAME = 'dataset_1'
 TABLE_NAME = 'table_1'
+
+BIGQUERY_SOURCE_CONFIG_DICT_1 = {
+    'projectName': PROJECT_NAME,
+    'sqlQuery': SQL_QUERY
+}
+
+PARAM_NAMES = ['param_name_1', 'param_name_2']
+
+SOURCE_CONFIG = {
+    'resource': RESOURCE,
+    'secrets': SECRETS
+}
 
 TARGET_CONFIG = {
     'projectName': PROJECT_NAME,
@@ -66,13 +75,46 @@ class TestTwitterAdsApiConfig:
         }))
         assert config.source.secrets.mapping['key1'] == secrets['key1']
 
-    def test_should_read_api_params(self):
-        params = {'param1': 'value1'}
+    def test_should_read_param_names_if_defined(self):
         config = TwitterAdsApiConfig.from_dict(get_config_for_item_config_dict({
             **ITEM_CONFIG_DICT,
             'source': {
                 **SOURCE_CONFIG,
-                'params': params
+                'paramNames': PARAM_NAMES
             }
         }))
-        assert config.source.params == params
+        assert config.source.param_names == PARAM_NAMES
+
+    def test_should_read_param_names_as_an_empty_list_if_not_defined(self):
+        config = TwitterAdsApiConfig.from_dict(CONFIG_DICT)
+        assert config.source.param_names == []
+
+    def test_should_read_source_bigquery_sql_query_if_defined(self):
+        config = TwitterAdsApiConfig.from_dict(get_config_for_item_config_dict({
+            **ITEM_CONFIG_DICT,
+            'source': {
+                **SOURCE_CONFIG,
+                'bigQuery': BIGQUERY_SOURCE_CONFIG_DICT_1
+            }
+        }))
+        assert config.source.bigquery.sql_query == SQL_QUERY
+
+    def test_should_read_source_bigquery_project_name_if_defined(self):
+        config = TwitterAdsApiConfig.from_dict(get_config_for_item_config_dict({
+            **ITEM_CONFIG_DICT,
+            'source': {
+                **SOURCE_CONFIG,
+                'bigQuery': BIGQUERY_SOURCE_CONFIG_DICT_1
+            }
+        }))
+        assert config.source.bigquery.project_name == PROJECT_NAME
+
+    def test_should_return_empty_str_for_bq_project_name_if_not_defined(self):
+        config = TwitterAdsApiConfig.from_dict(CONFIG_DICT)
+        print(config.source.bigquery)
+        assert config.source.bigquery.project_name == ''
+
+    def test_should_return_empty_str_for_bq_sql_query_if_not_defined(self):
+        config = TwitterAdsApiConfig.from_dict(CONFIG_DICT)
+        print(config.source.bigquery)
+        assert config.source.bigquery.sql_query == ''
