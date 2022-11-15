a50920c7c464614950965c393a3655a39e850025
HazalCiplak <hazalciplak@gmail.com>
2022-11-15 15:46:26 +0000

    fized the type of the fixture

--- tests/unit_test/conftest.py
+++ tests/unit_test/conftest.py
@@ -1,7 +1,7 @@
 import logging
 from pathlib import Path
 from unittest.mock import patch
-from typing import ContextManager
+from typing import ContextManager, Iterable
 
 import pytest
 from py._path.local import LocalPath
@@ -20,6 +20,6 @@ def temp_dir(tmpdir: LocalPath) -> Path:
 
 
 @pytest.fixture()
-def mock_env() -> ContextManager[dict]:
+def mock_env() -> Iterable[ContextManager[dict]]:
     with patch('os.environ', {}) as env_dict:
         yield env_dict
