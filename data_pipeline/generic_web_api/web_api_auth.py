340a4d608cc2f662144443443c935332e40aebf3
HazalCiplak <hazalciplak@gmail.com>
2022-11-15 11:37:22 +0000

    casted type of auth_val_list to Tuple[str, str]

--- data_pipeline/generic_web_api/web_api_auth.py
+++ data_pipeline/generic_web_api/web_api_auth.py
@@ -1,5 +1,5 @@
 import os
-from typing import Optional
+from typing import Tuple, cast
 
 from data_pipeline.utils.pipeline_file_io import read_file_content
 
@@ -8,14 +8,15 @@ class WebApiAuthentication:
     def __init__(
             self,
             auth_type: str,
-            auth_param_val_list: Optional[list] = None,
+            auth_param_val_list: list,
     ):
         self.authentication_type = auth_type.lower()
-
-        self.auth_val_list = [
+        if auth_type == 'basic':
+            assert len(auth_param_val_list) == 2
+        self.auth_val_list = cast([
             get_auth_param_value(auth_val_conf)
             for auth_val_conf in auth_param_val_list
-        ] if auth_type == 'basic' else None
+        ], Tuple[str, str]) if auth_type == 'basic' else None
 
 
 def get_auth_param_value(auth_val_conf: dict):
