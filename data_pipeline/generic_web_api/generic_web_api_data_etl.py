c5cc3b1c0ecb816e564a5d1a4b3f8f5028a24cfa
Daniel Ecer <de-code@users.noreply.github.com>
2023-11-13 17:27:22 +0000

    Inlined initial cursor

--- data_pipeline/generic_web_api/generic_web_api_data_etl.py
+++ data_pipeline/generic_web_api/generic_web_api_data_etl.py
@@ -215,14 +215,13 @@ def process_web_api_data_etl_batch(
             ModuleConstant.DEFAULT_TIMESTAMP_FORMAT
         )
     )
-    cursor = None
     latest_record_timestamp = None
     current_url_compose_arg: Optional[UrlComposeParam] = UrlComposeParam(
         from_date=from_date_to_advance or initial_from_date,
         to_date=get_next_until_date(
             from_date_to_advance, data_config, until_date
         ),
-        cursor=cursor,
+        cursor=None,
         page_number=1 if data_config.url_builder.page_number_param else None,
         page_offset=0 if data_config.url_builder.offset_param else None,
         source_values=source_values
