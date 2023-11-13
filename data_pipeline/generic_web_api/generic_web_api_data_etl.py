3f0b04d9729c74dc4a5ea2f404a6ed8317ad2ddc
Daniel Ecer <de-code@users.noreply.github.com>
2023-11-13 17:23:28 +0000

    Inline initial page_number

--- data_pipeline/generic_web_api/generic_web_api_data_etl.py
+++ data_pipeline/generic_web_api/generic_web_api_data_etl.py
@@ -221,12 +221,11 @@ def process_web_api_data_etl_batch(
         from_date_to_advance, data_config, until_date
     )
     offset = 0 if data_config.url_builder.offset_param else None
-    page_number = 1 if data_config.url_builder.page_number_param else None
     current_url_compose_arg: Optional[UrlComposeParam] = UrlComposeParam(
         from_date=from_date_to_advance or initial_from_date,
         to_date=variable_until_date,
         cursor=cursor,
-        page_number=page_number,
+        page_number=1 if data_config.url_builder.page_number_param else None,
         page_offset=offset,
         source_values=source_values
     )
