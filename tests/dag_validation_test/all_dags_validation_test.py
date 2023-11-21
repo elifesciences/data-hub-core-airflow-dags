46c73a0c978d0d0c5622be20978758c5e2221619
Daniel Ecer <de-code@users.noreply.github.com>
2023-11-21 17:54:57 +0000

    log vars when DAG wasn't found

--- tests/dag_validation_test/all_dags_validation_test.py
+++ tests/dag_validation_test/all_dags_validation_test.py
@@ -1,3 +1,4 @@
+import logging
 import importlib
 import os
 
@@ -8,21 +9,30 @@ from airflow.utils.dag_cycle_tester import test_cycle as _test_cycle
 from tests.dag_validation_test.conftest import DAG_FILES, DAG_PATH
 
 
+LOGGER = logging.getLogger(__name__)
+
+
 @pytest.mark.parametrize("dag_file", DAG_FILES)
 def test_dag_should_contain_no_cycle(dag_file):
     module_name, _ = os.path.splitext(dag_file)
     module_path = os.path.join(DAG_PATH, dag_file)
 
     mod_spec = importlib.util.spec_from_file_location(
-        module_name, module_path)
+        module_name, module_path
+    )
 
     module = importlib.util.module_from_spec(mod_spec)
 
     mod_spec.loader.exec_module(module)
 
+    module_vars = vars(module)
     dag_objects = [
-        var for var in vars(module).values() if isinstance(
-            var, af_models.DAG)]
+        var
+        for var in module_vars.values()
+        if isinstance(var, af_models.DAG)
+    ]
+    if not dag_objects:
+        LOGGER.error('no instance of DAG found in: %r', module_vars.keys())
     assert len(dag_objects) > 0
 
     for dag in dag_objects:
