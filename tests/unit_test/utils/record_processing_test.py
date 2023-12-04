8277b51c0305ba61caf07e8c19a531faeadf06bc
Daniel Ecer <de-code@users.noreply.github.com>
2023-12-04 23:08:27 +0000

    Create test classes around record processing tests

--- tests/unit_test/utils/record_processing_test.py
+++ tests/unit_test/utils/record_processing_test.py
@@ -5,53 +5,55 @@ from data_pipeline.utils.record_processing import (
 )
 
 
-def test_should_unescape_html_should_return_unescaped_values():
-    test_data = ["&quot;", "&amp;", "&lt", "&gt"]
-    expected_result = ["\"", "&", "<", ">"]
-    result = [
-        unescape_html_escaped_values_in_string(val)
-        for val in test_data
-    ]
-    assert result == expected_result
+class TestUnescapeHtmlEscapedValuesInString:
+    def test_should_unescape_html_should_return_unescaped_values(self):
+        test_data = ["&quot;", "&amp;", "&lt", "&gt"]
+        expected_result = ["\"", "&", "<", ">"]
+        result = [
+            unescape_html_escaped_values_in_string(val)
+            for val in test_data
+        ]
+        assert result == expected_result
 
 
-def test_should_strip_when_quote_is_at_both_ends():
-    test_data = [" 'test1' ", "\"test2\""]
-    expected_result = ["test1", "test2"]
-    result = [
-        strip_quotes(val)
-        for val in test_data
-    ]
-    assert result == expected_result
+class TestStripQuotes:
+    def test_should_strip_when_quote_is_at_both_ends(self):
+        test_data = [" 'test1' ", "\"test2\""]
+        expected_result = ["test1", "test2"]
+        result = [
+            strip_quotes(val)
+            for val in test_data
+        ]
+        assert result == expected_result
+
+    def test_should_not_strip_when_quote_is_at_only_one_end(self):
+        test_data = [" 'test1 ", "test2\""]
+        expected_result = ["'test1", "test2\""]
+        result = [
+            strip_quotes(val)
+            for val in test_data
+        ]
+        assert result == expected_result
 
 
-def test_should_not_strip_when_quote_is_at_only_one_end():
-    test_data = [" 'test1 ", "test2\""]
-    expected_result = ["'test1", "test2\""]
-    result = [
-        strip_quotes(val)
-        for val in test_data
-    ]
-    assert result == expected_result
-
-
-def test_should_unescape_html_for_nested_record():
-    data = {
-        "a": "&pound;682m",
-        "b": {
-            "c": "d",
-            "e": "&pound;682m",
-            "f": ["a", "y", "&pound;682m", "z"],
-            "g": [{"f": "k", "m": "&pound;682m"}, {"y": "l"}]
+class TestProcessRecordValues:
+    def test_should_unescape_html_for_nested_record(self):
+        data = {
+            "a": "&pound;682m",
+            "b": {
+                "c": "d",
+                "e": "&pound;682m",
+                "f": ["a", "y", "&pound;682m", "z"],
+                "g": [{"f": "k", "m": "&pound;682m"}, {"y": "l"}]
+            }
         }
-    }
-    expected_result = {
-        'a': '£682m',
-        'b': {
-            'c': 'd', 'e': '£682m',
-            'f': ['a', 'y', '£682m', 'z'],
-            'g': [{'f': 'k', 'm': '£682m'}, {'y': 'l'}]
+        expected_result = {
+            'a': '£682m',
+            'b': {
+                'c': 'd', 'e': '£682m',
+                'f': ['a', 'y', '£682m', 'z'],
+                'g': [{'f': 'k', 'm': '£682m'}, {'y': 'l'}]
+            }
         }
-    }
-    actual_result = process_record_values(data, ["html_unescape"])
-    assert actual_result == expected_result
+        actual_result = process_record_values(data, ["html_unescape"])
+        assert actual_result == expected_result
