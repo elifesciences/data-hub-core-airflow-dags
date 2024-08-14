5ed67c5bf03e3361079adc101d4377acfd66e3c0
HazalCiplak <hazalciplak@gmail.com>
2024-08-14 14:19:37 +0100

    created pipelines from the config

--- dags/kubernetes_pipeline.py
+++ dags/kubernetes_pipeline.py
@@ -7,30 +7,46 @@ from typing import Sequence
 import airflow
 from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator
 
+from data_pipeline.kubernetes.kubernetes_pipeline_config import MultiKubernetesPipelineConfig
 from data_pipeline.utils.dags.data_pipeline_dag_utils import (
     create_dag,
+)
+from data_pipeline.utils.pipeline_config import (
+    get_pipeline_config_for_env_name_and_config_parser
 )
 
 LOGGER = logging.getLogger(__name__)
 
 
+class KubernetesPipelineConfigEnvironmentVariables:
+    CONFIG_FILE_PATH = 'KUBERNETES_PIPELINE_CONFIG_FILE_PATH'
+
+
+def get_multi_kubernetes_pipeline_config() -> MultiKubernetesPipelineConfig:
+    return get_pipeline_config_for_env_name_and_config_parser(
+        KubernetesPipelineConfigEnvironmentVariables.CONFIG_FILE_PATH,
+        MultiKubernetesPipelineConfig.from_dict
+    )
+
+
 def create_kubernetes_pipeline_dags() -> Sequence[airflow.DAG]:
     dags = []
-    with create_dag(
-        dag_id='kubernetes_pipeline',
-        dagrun_timeout=timedelta(days=1),
-        tags=['Kubernetes']
-    ) as dag:
-        KubernetesPodOperator(
-            name='hello-dry-run',
-            image='debian',
-            cmds=['bash', '-cx'],
-            arguments=['sleep 60'],
-            labels={'foo': 'bar'},
-            task_id='dry_run_demo',
-            do_xcom_push=False
-        )
-        dags.append(dag)
+    multi_kubernetes_pipeline_config = get_multi_kubernetes_pipeline_config()
+    for kubernetes_pipeline_config in multi_kubernetes_pipeline_config.kubernetes_pipelines:
+        with create_dag(
+            dag_id=kubernetes_pipeline_config.data_pipeline_id,
+            dagrun_timeout=timedelta(days=1),
+            tags=['Kubernetes']
+        ) as dag:
+            KubernetesPodOperator(
+                task_id='dry_run_demo',
+                name='hello-dry-run',
+                image='debian',
+                arguments=['sleep 60'],
+                do_xcom_push=False
+            )
+            dags.append(dag)
+
     return dags
 
 
