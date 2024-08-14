9c68bdb27bfd3c29e9be0de48b140540bca8bfe5
Daniel Ecer <de-code@users.noreply.github.com>
2024-08-14 08:56:12 +0100

    Added mount to the code

--- dags/docker_pipeline.py
+++ dags/docker_pipeline.py
@@ -2,10 +2,13 @@
 
 from datetime import timedelta
 import logging
+import os
 from typing import Sequence
 
 import airflow
 from airflow.providers.docker.operators.docker import DockerOperator
+
+from docker.types import Mount
 
 from data_pipeline.utils.dags.data_pipeline_dag_utils import (
     create_dag,
@@ -14,8 +17,13 @@ from data_pipeline.utils.dags.data_pipeline_dag_utils import (
 LOGGER = logging.getLogger(__name__)
 
 
+def get_container_host_project_path() -> str:
+    return os.environ['CONTAINER_HOST_PROJECT_PATH']
+
+
 def create_docker_pipeline_dags() -> Sequence[airflow.DAG]:
     dags = []
+    container_host_project_path = get_container_host_project_path()
     with create_dag(
         dag_id='docker_pipeline',
         dagrun_timeout=timedelta(days=1),
@@ -24,11 +32,24 @@ def create_docker_pipeline_dags() -> Sequence[airflow.DAG]:
     ) as dag:
         image = 'elifesciences/data-hub-core-dags-dev'
         LOGGER.info('image: %r', image)
+        mounts = [
+            Mount(
+                target='/opt/airflow/data_pipeline',
+                source=f'{container_host_project_path}/data_pipeline',
+                type='bind',
+                read_only=True
+            )
+        ]
+        LOGGER.info('mounts: %r', mounts)
+
         DockerOperator(
             container_name="hello-dry-run",
             image=image,
             command=["bash", "-cx", "sleep 60"],
             task_id="dry_run_demo",
+            mounts=mounts,
+            mount_tmp_dir=False,
+            auto_remove='force',
             do_xcom_push=False
         )
         dags.append(dag)
