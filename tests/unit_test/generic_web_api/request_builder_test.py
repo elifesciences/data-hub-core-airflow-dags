elifesciences/data-hub-core-airflow-dags

commit dbf1ac61bc2992d377be2a8a91a3cf9fc3cb187d

Web API: OpenAlex/Crossref: Combine static filters with dynamic ones (#1547)
* Web API: OpenAlex/Crossref: Combine static filters with dynamic filters

part of https://github.com/elifesciences/data-hub-issues/issues/995

Date range parameters would be expressed as expressions within `filter` parameters. There may also be static filter parameters specified as part of the URL.

With this PR those parameters would get combined.

e.g. for `urlExcludingConfigurableParameters` configured as `https://api.openalex.org/works?filter=type:preprint`
and adding `from_publication_date` and `to_publication_date` dynamic parameters, the url could result in something like `https://api.openalex.org/works?filter=type:preprint,from_publication_date=2001-01-01,to_publication_date=2001-01-02`.

* Updated sample config to chunk by publication date

* Removed commented out urlunparse function

* Removed no longer used _get_url_separator

* Extracted get_non_empty_parameters for readability

* inlined get_non_empty_parameters

* Renamed to params_from_url

* Extracted get_url_with_added_or_replaced_query_parameters for readability

* Inlined url

* Put get_non_empty_parameters back in

data_pipeline/generic_web_api/request_builder.py
@@ -29,6 +29,30 @@ class WebApiDynamicRequestParameters(NamedTuple):
     placeholder_values: Optional[dict] = None
 
 
+def get_non_empty_parameters(parameters: dict) -> dict:
+    return {
+        key: value
+        for key, value in parameters.items()
+        if key and value
+    }
+
+
+def get_url_with_added_or_replaced_query_parameters(
+    url: str,
+    parameters: dict
+) -> str:
+    parsed_url = parse.urlparse(url)
+    params_from_url = parse.parse_qs(parsed_url.query)
+    combined_query_params = {
+        **params_from_url,
+        **parameters
+    }
+    LOGGER.debug('combined_query_params: %r', combined_query_params)
+    return parse.urlunparse(
+        parsed_url._replace(query=parse.urlencode(combined_query_params))
+    )
+
+
 # pylint: disable=too-many-instance-attributes,too-many-arguments
 @dataclass(frozen=True)
 class WebApiDynamicRequestBuilder:
@@ -56,31 +80,20 @@ class WebApiDynamicRequestBuilder:
     ) -> Optional[Any]:
         return None
 
-    def _get_url_separator(self) -> str:
-        url = self.url_excluding_configurable_parameters
-        if "?" in url:
-            if url.strip().endswith("&") or url.strip().endswith("?"):
-                url_separator = ""
-            else:
-                url_separator = "&"
-        else:
-            url_separator = "?"
-        return url_separator
-
     def compose_url(
         self,
         parameters_key_value: dict,
         placeholder_values: Optional[dict] = None
     ) -> str:
-        url = self.url_excluding_configurable_parameters
-        url_separator = self._get_url_separator()
-        params = parse.urlencode(
-            {
-                key: value
-                for key, value in parameters_key_value.items() if key and value
-            }
+        composed_url = get_url_with_added_or_replaced_query_parameters(
+            url=replace_placeholders(
+                self.url_excluding_configurable_parameters,
+                placeholder_values
+            ),
+            parameters=get_non_empty_parameters(parameters_key_value)
         )
-        return replace_placeholders(url, placeholder_values) + url_separator + params
+        LOGGER.debug('composed_url: %r', composed_url)
+        return composed_url
 
     def get_url(
         self,
@@ -220,6 +233,10 @@ class CrossrefMetadataWebApiDynamicRequestBuilder(WebApiDynamicRequestBuilder):
         end_date = datetime_to_string(
             dynamic_request_parameters.to_date, self.date_format
         )
+        parsed_url = parse.urlparse(self.url_excluding_configurable_parameters)
+        parsed_qs = parse.parse_qs(parsed_url.query)
+        static_filter_expression = parsed_qs.get('filter') or []
+        LOGGER.debug('static_filter_expression: %r', static_filter_expression)
         filter_dict = {
             key: value
             for key, value in [
@@ -227,7 +244,7 @@ class CrossrefMetadataWebApiDynamicRequestBuilder(WebApiDynamicRequestBuilder):
                 (self.to_date_param, end_date),
             ] if key and value
         }
-        filter_value = ','.join([
+        filter_value = ','.join(static_filter_expression + [
             f'{key}:{value}'
             for key, value in filter_dict.items()
         ])

tests/unit_test/generic_web_api/request_builder_test.py
@@ -135,6 +135,25 @@ class TestCrossrefMetadataWebApiDynamicRequestBuilder:
         params = parse_qs(url.query)
         assert params.get('filter') == ['from-index-date:2024-01-29,until-index-date:2024-01-30']
 
+    def test_should_add_from_date_as_filter_parameter_to_existing_filter_parameter_in_url(self):
+        dynamic_request_builder = CrossrefMetadataWebApiDynamicRequestBuilder(
+            url_excluding_configurable_parameters=(
+                'https://test/api1?filter=existing-filter1:value1'
+            ),
+            from_date_param='from-index-date',
+            date_format=r'%Y-%m-%d',
+            static_parameters={}
+        )
+        LOGGER.debug('dynamic_request_builder: %r', dynamic_request_builder)
+        url = urlparse(dynamic_request_builder.get_url(
+            dynamic_request_parameters=WebApiDynamicRequestParameters(
+                from_date=datetime.fromisoformat('2024-01-29+00:00')
+            )
+        ))
+        LOGGER.debug('url: %r', url)
+        params = parse_qs(url.query)
+        assert params.get('filter') == ['existing-filter1:value1,from-index-date:2024-01-29']
+
     def test_should_replace_placeholders(self):
         dynamic_request_builder = CrossrefMetadataWebApiDynamicRequestBuilder(
             url_excluding_configurable_parameters=TEST_API_URL_1 + '/{placeholder}',

sample_data_config/web-api/web-api-data-pipeline.config.yaml
@@ -133,16 +133,25 @@ webApi:
   - dataPipelineId: openalex_works_metadata_for_preprints
     description:
       Retrieve OpenAlex metadata for all of the preprints.
-      Currently this will get all of the preprints every time because we do not yet have premium access.
+      Currently this will use the publication date to chunk data.
+      It is recommended to use the `updated_date` which requires premium access.
     dataset: '{ENV}'
     table: test_openalex_works_api_response
+    stateFile:
+      bucketName: '{ENV}-elife-data-pipeline'
+      objectName: 'airflow-config/generic-web-api/{ENV}-openalex-preprints-by-publication-date.json'
     urlSourceType:
       # Note: We currently need to use `crossref_metadata_api` for the cursor parameter to work
       name: 'crossref_metadata_api'
     dataUrl:
-      urlExcludingConfigurableParameters: 'https://api.openalex.org/works?filter=type:preprint,publication_date:2024-02-01'
+      urlExcludingConfigurableParameters: 'https://api.openalex.org/works?filter=type:preprint'
       configurableParameters:
         nextPageCursorParameterName: 'cursor'
+        fromDateParameterName: 'from_publication_date'
+        toDateParameterName: 'to_publication_date'
+        daysDiffFromStartTillEnd: 10  # load data one day at a time
+        defaultStartDate: '2024-10-01+00:00'
+        dateFormat: '%Y-%m-%d'
         pageSizeParameterName: 'per-page'
         defaultPageSize: 200
     response:
@@ -156,7 +165,7 @@ webApi:
         - count
       recordTimestamp:
         itemTimestampKeyFromItemRoot:
-          - 'updated_date'
+          - 'publication_date'
       provenanceEnabled: True
       fieldsToReturn:
         - id

commit ea223da2c8358de81a1ac1e502bc9fb497fbf2ec

Fixed Web API existing url parameter handling
The bug was introduced by https://github.com/elifesciences/data-hub-core-airflow-dags/pull/1547

data_pipeline/generic_web_api/request_builder.py
@@ -44,7 +44,7 @@ def get_url_with_added_or_replaced_query_parameters(
     }
     LOGGER.debug('combined_query_params: %r', combined_query_params)
     return parse.urlunparse(
-        parsed_url._replace(query=parse.urlencode(combined_query_params))
+        parsed_url._replace(query=parse.urlencode(combined_query_params, doseq=True))
     )
 
 

tests/unit_test/generic_web_api/request_builder_test.py
@@ -5,6 +5,7 @@ from urllib.parse import parse_qs, urlparse
 from data_pipeline.generic_web_api.request_builder import (
     CrossrefMetadataWebApiDynamicRequestBuilder,
     S2TitleAbstractEmbeddingsWebApiDynamicRequestBuilder,
+    get_url_with_added_or_replaced_query_parameters,
     get_web_api_request_builder_class,
     BioRxivWebApiDynamicRequestBuilder,
     WebApiDynamicRequestParameters
@@ -15,6 +16,26 @@ LOGGER = logging.getLogger(__name__)
 
 
 TEST_API_URL_1 = 'https://test/api1'
+
+
+class TestGetUrlWithAddedOrReplacedQueryParameters:
+    def test_should_add_parameter(self):
+        assert get_url_with_added_or_replaced_query_parameters(
+            'https://test/api1',
+            parameters={'param1': 'value1'}
+        ) == 'https://test/api1?param1=value1'
+
+    def test_should_replace_parameter(self):
+        assert get_url_with_added_or_replaced_query_parameters(
+            'https://test/api1?param1=old-value',
+            parameters={'param1': 'new-value'}
+        ) == 'https://test/api1?param1=new-value'
+
+    def test_should_keep_existing_parameter(self):
+        assert get_url_with_added_or_replaced_query_parameters(
+            'https://test/api1?existing-param=old-value',
+            parameters={'new-param': 'new-value'}
+        ) == 'https://test/api1?existing-param=old-value&new-param=new-value'
 
 
 class TestDynamicBioRxivMedRxivURLBuilder: