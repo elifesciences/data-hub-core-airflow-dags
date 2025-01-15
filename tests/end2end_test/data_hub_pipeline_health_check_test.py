45ef5ca309c1c2a7005579fce075bd0d07200e8e
Daniel Ecer <de-code@users.noreply.github.com>
2025-01-15 14:16:09 +0000

    Fixed loading config for end to end test

--- tests/end2end_test/data_hub_pipeline_health_check_test.py
+++ tests/end2end_test/data_hub_pipeline_health_check_test.py
@@ -1,19 +1,33 @@
+import logging
+import os
 from data_pipeline.monitoring.data_hub_pipeline_health_check import (
     run_data_hub_pipeline_health_check
 )
 from data_pipeline.monitoring.monitoring_config import MonitoringConfig
 from data_pipeline.utils.pipeline_config import (
-    get_pipeline_config_for_env_name_and_config_parser
+    get_deployment_env
 )
+from data_pipeline.utils.pipeline_file_io import get_yaml_file_as_dict
+
+
+LOGGER = logging.getLogger(__name__)
 
 
 MONITORING_CONFIG_FILE_PATH_ENV_NAME = "MONITORING_CONFIG_FILE_PATH"
 
 
 def test_run_data_hub_pipeline_health_check():
-    data_config = get_pipeline_config_for_env_name_and_config_parser(
-        MONITORING_CONFIG_FILE_PATH_ENV_NAME,
-        MonitoringConfig
+    config_file_path = os.environ[
+        MONITORING_CONFIG_FILE_PATH_ENV_NAME
+    ]
+    LOGGER.info('conf_file_path: %s', config_file_path)
+    data_config_dict = get_yaml_file_as_dict(
+        config_file_path
+    )
+    deployment_env_var = get_deployment_env()
+    data_config = MonitoringConfig(
+        data_config_dict,
+        deployment_env_var
     )
     run_data_hub_pipeline_health_check(
         project=data_config.project_name,
