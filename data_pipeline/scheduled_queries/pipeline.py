7bea468186fc48990ce1847d67494fb9c3edf823
Daniel Ecer <de-code@users.noreply.github.com>
2025-02-27 16:55:52 +0000

    Log time it took

--- data_pipeline/scheduled_queries/pipeline.py
+++ data_pipeline/scheduled_queries/pipeline.py
@@ -1,4 +1,5 @@
 import logging
+import time
 
 from data_pipeline.utils.data_store.bq_data_service import (
     get_bq_client
@@ -16,12 +17,21 @@ LOGGER = logging.getLogger(__name__)
 def process_scheduled_query(pipeline_config: ScheduledQueryPipelineConfig):
     LOGGER.info('pipeline_config: %r', pipeline_config)
     LOGGER.info('Running SQL Query: %r', pipeline_config.bigquery.sql_query)
+
+    start_time = time.perf_counter()
+
     client = get_bq_client(project=pipeline_config.bigquery.project_name)
     query_job = client.query(pipeline_config.bigquery.sql_query)
     query_job.result()
 
+    duration = time.perf_counter() - start_time
+
     total_bytes_billed = query_job.total_bytes_billed
-    LOGGER.info('total_bytes_billed: %r', total_bytes_billed)
+    LOGGER.info(
+        'Scheduled Query: %s bytes billed, took: %.3fs',
+        total_bytes_billed,
+        duration
+    )
 
 
 def process_scheduled_queries(multi_config: MultiScheduledQueryPipelineConfig):
