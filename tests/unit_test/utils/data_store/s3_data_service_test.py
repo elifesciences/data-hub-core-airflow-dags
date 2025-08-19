77abd37555ac16b7b6e846901c4d1402588ecf40
Daniel Ecer <de-code@users.noreply.github.com>
2025-08-19 12:50:36 +0100

    Added test_should_return_file_metadata_with_object_pattern

--- tests/unit_test/utils/data_store/s3_data_service_test.py
+++ tests/unit_test/utils/data_store/s3_data_service_test.py
@@ -9,6 +9,8 @@ from botocore.compat import six
 from botocore.response import StreamingBody
 
 from data_pipeline.utils.data_store.s3_data_service import (
+    FileMetadata,
+    FileMetadataWithObjectPattern,
     download_s3_yaml_object_as_json,
     iter_sorted_new_s3_files_to_process,
     s3_open_binary_read_with_temp_file,
@@ -29,7 +31,15 @@ TIMESTAMP_2 = datetime.fromisoformat(TIMESTAMP_STRING_2)
 
 S3_BUCKET_NAME_1 = 's3_bucket_name_1'
 
-OBJECT_PATTERN_1 = 'object_pattern_1'
+OBJECT_PATTERN_1 = 'object_pattern_1*'
+
+OBJECT_KEY_1 = 'object_key_1'
+
+FILE_METADATA_1 = FileMetadata(
+    bucket=S3_BUCKET_NAME_1,
+    name=OBJECT_KEY_1,
+    last_modified=TIMESTAMP_1
+)
 
 
 @pytest.fixture(name="mock_s3_client_function", autouse=True)
@@ -160,3 +170,20 @@ class TestIterSortedNewS3FilesToProcess:
             pattern=OBJECT_PATTERN_1,
             latest_timestamp=TIMESTAMP_1
         )
+
+    def test_should_return_file_metadata_with_object_pattern(
+        self,
+        mock_list_objects_with_pattern_and_timestamp: MagicMock
+    ):
+        mock_list_objects_with_pattern_and_timestamp.return_value = [
+            FILE_METADATA_1
+        ]
+        assert list(iter_sorted_new_s3_files_to_process(
+            obj_pattern_with_latest_dates={OBJECT_PATTERN_1: TIMESTAMP_1},
+            s3_bucket_name=S3_BUCKET_NAME_1
+        )) == [
+            FileMetadataWithObjectPattern(
+                file_metadata=FILE_METADATA_1,
+                object_key_pattern=OBJECT_PATTERN_1
+            )
+        ]
