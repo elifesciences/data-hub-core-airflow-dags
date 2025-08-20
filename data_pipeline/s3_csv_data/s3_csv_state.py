5761e5d8ffee985069e05211532250f59222c603
Daniel Ecer <de-code@users.noreply.github.com>
2025-08-20 12:54:32 +0100

    implemented CsvState.from_dict

--- data_pipeline/s3_csv_data/s3_csv_state.py
+++ data_pipeline/s3_csv_data/s3_csv_state.py
@@ -1,12 +1,40 @@
 from dataclasses import dataclass
 from datetime import datetime
+from typing import Mapping
+
+from dateutil import tz
+
+
+DATETIME_FORMAT = r"%Y-%m-%d %H:%M:%S"
+
+
+def convert_datetime_string_to_datetime(
+    datetime_as_string: str
+) -> datetime:
+    tz_unaware = datetime.strptime(datetime_as_string.strip(), DATETIME_FORMAT)
+    tz_aware = tz_unaware.replace(tzinfo=tz.tzlocal())
+
+    return tz_aware
 
 
 @dataclass(frozen=True)
 class ObjectPatternCsvState:
-    last_modified_timestamp: datetime
+    last_modified_datetime: datetime
 
 
 @dataclass(frozen=True)
 class CsvState:
     state_dict: dict[str, ObjectPatternCsvState]
+
+    @staticmethod
+    def from_dict(state_dict: Mapping[str, str]) -> 'CsvState':
+        return CsvState(
+            state_dict={
+                object_pattern: ObjectPatternCsvState(
+                    last_modified_datetime=convert_datetime_string_to_datetime(
+                        datetime_str
+                    )
+                )
+                for object_pattern, datetime_str in state_dict.items()
+            }
+        )
---
+++ tests/unit_test/s3_csv_data/s3_csv_state_test.py
@@ -0,0 +1,25 @@
+from data_pipeline.s3_csv_data.s3_csv_etl import convert_datetime_string_to_datetime
+from data_pipeline.s3_csv_data.s3_csv_state import CsvState, ObjectPatternCsvState
+
+
+DATETTIME_STRING_1 = '2020-01-01 00:00:00'
+
+DATETTIME_1 = convert_datetime_string_to_datetime(DATETTIME_STRING_1)
+
+OBJECT_PATTERN_1 = 'object_pattern_1*'
+
+
+class TestCsvState:
+    def test_should_load_from_empty_dict(self):
+        state = CsvState.from_dict({})
+        assert not state.state_dict
+
+    def test_should_parse_timestamp(self):
+        state = CsvState.from_dict({
+            OBJECT_PATTERN_1: DATETTIME_STRING_1
+        })
+        assert state.state_dict == {
+            OBJECT_PATTERN_1: ObjectPatternCsvState(
+                last_modified_datetime=DATETTIME_1
+            )
+        }
