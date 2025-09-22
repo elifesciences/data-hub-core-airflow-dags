53bfb9830610b1de83b1ac2ba73fe0efff635266
Daniel Ecer <de-code@users.noreply.github.com>
2025-09-22 12:11:54 +0100

    Moved standardize_field_name above get_standardized_csv_header

--- data_pipeline/google_spreadsheet/google_spreadsheet_etl.py
+++ data_pipeline/google_spreadsheet/google_spreadsheet_etl.py
@@ -64,6 +64,10 @@ def get_record_metadata(
     return record_metadata
 
 
+def standardize_field_name(field_name: str):
+    return re.sub(r"\W", "_", field_name.strip().strip('"').strip("'"))
+
+
 def get_standardized_csv_header(csv_header):
     return [
         standardize_field_name(field.lower())
@@ -177,10 +181,6 @@ def etl_google_spreadsheet(spreadsheet_config: MultiCsvSheet):
                 full_temp_file_location,
                 current_timestamp_as_str
             )
-
-
-def standardize_field_name(field_name: str):
-    return re.sub(r"\W", "_", field_name.strip().strip('"').strip("'"))
 
 
 def process_record(record: list,
