b9a04b7d0fb8670b671ffd67fd1383bf55544aba
HazalCiplak <hazalciplak@gmail.com>
2025-10-07 09:01:54 +0100

    Timestamp handling in Google Spreadsheet ETL: rename last_checked_timestamp to last_checked_timestamp_str and update related logic

--- data_pipeline/google_spreadsheet/google_spreadsheet_etl.py
+++ data_pipeline/google_spreadsheet/google_spreadsheet_etl.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import logging
 import re
 from tempfile import TemporaryDirectory
@@ -23,6 +24,7 @@ from data_pipeline.utils.csv.metadata_schema import (
     extend_nested_table_schema_if_new_fields_exist,
 )
 from data_pipeline.utils.data_pipeline_timestamp import (
+    get_current_timestamp,
     get_current_timestamp_as_string,
     parse_timestamp
 )
@@ -162,13 +164,13 @@ def should_autodetect_schema(
 
 def update_last_checked_timestamp_for_all_rows(
     csv_sheet_config: BaseCsvSheetConfig,
-    last_checked_timestamp: str,
+    last_checked_timestamp_str: str,
 ):
     client = get_bq_client(csv_sheet_config.gcp_project)
 
     if should_autodetect_schema(
         csv_sheet_config,
-        standardized_csv_header=['last_checked_timestamp'],
+        standardized_csv_header=['last_checked_timestamp_str'],
     ):
         return
 
@@ -180,7 +182,7 @@ def update_last_checked_timestamp_for_all_rows(
 
     sql = f"""
         UPDATE `{table_ref}`
-        SET last_checked_timestamp = '{last_checked_timestamp}'
+        SET last_checked_timestamp_str = '{last_checked_timestamp_str}'
         WHERE 1=1
     """
 
@@ -297,7 +299,7 @@ def etl_google_spreadsheet(spreadsheet_config: MultiCsvSheetConfig):
                     csv_sheet_config=spreadsheet_config.sheets_config[
                         list(spreadsheet_config.sheets_config.keys())[0]
                     ],
-                    last_checked_timestamp=current_timestamp_as_str,
+                    last_checked_timestamp_str=current_timestamp_as_str,
                 )
                 return
         except FileNotFoundError:
