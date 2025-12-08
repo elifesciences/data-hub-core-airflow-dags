271a0e58222f5273bed4be3b3f8ff9e3b055628e
HazalCiplak <hazalciplak@gmail.com>
2025-12-08 10:43:38 +0000

    Added initial Gmail CLI and new pipeline module

---
+++ data_pipeline/gmail_data/cli.py
@@ -0,0 +1,25 @@
+import argparse
+import logging
+from data_pipeline.gmail_data.gmail_data_pipeline import get_multi_gmail_data_config
+
+LOGGER = logging.getLogger(__name__)
+
+
+def gmail_data_etl(data_pipeline_id: str):
+    config = get_multi_gmail_data_config()
+    LOGGER.info('Completed ETL for pipeline %s with config: %s', data_pipeline_id, config)
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Run ETL for a specific Web API pipeline")
+    parser.add_argument('--data-pipeline-id', required=True)
+    args = parser.parse_args()
+    data_pipeline_id = args.data_pipeline_id
+    LOGGER.info('Starting ETL for pipeline: %s', data_pipeline_id)
+    gmail_data_etl(data_pipeline_id)
+    LOGGER.info('ETL process completed successfully.')
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    main()
---
+++ data_pipeline/gmail_data/gmail_data_pipeline.py
@@ -0,0 +1,20 @@
+import logging
+
+from data_pipeline.gmail_data.get_gmail_data_config import (
+    MultiGmailDataConfig
+)
+from data_pipeline.utils.pipeline_config import get_pipeline_config_for_env_name_and_config_parser
+
+
+LOGGER = logging.getLogger(__name__)
+
+
+class GmailPipelineEnvironmentVariables:
+    CONFIG_FILE_PATH = 'GMAIL_DATA_CONFIG_FILE_PATH'
+
+
+def get_multi_gmail_data_config() -> MultiGmailDataConfig:
+    return get_pipeline_config_for_env_name_and_config_parser(
+        GmailPipelineEnvironmentVariables.CONFIG_FILE_PATH,
+        MultiGmailDataConfig
+    )
