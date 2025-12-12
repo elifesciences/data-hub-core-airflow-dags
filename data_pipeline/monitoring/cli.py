bf5c6c4dd3e4980c12a13af9ea68b26105c505ce
HazalCiplak <hazalciplak@gmail.com>
2025-12-12 15:54:38 +0000

    Implement monitoring CLI and update MonitoringConfig structure

--- data_pipeline/monitoring/cli.py
+++ data_pipeline/monitoring/cli.py
@@ -1,9 +1,41 @@
 import logging
 
+from data_pipeline.monitoring.monitoring_config import MonitoringConfig
+from data_pipeline.monitoring.data_hub_pipeline_health_check import (
+    run_data_hub_pipeline_health_check
+)
+from data_pipeline.monitoring.ping_healthchecks import ping
+from data_pipeline.utils.pipeline_config import (
+    get_pipeline_config_for_env_name_and_config_parser,
+)
+
 LOGGER = logging.getLogger(__name__)
 
 
+class MonitoringEnvironmentVariables:
+    CONFIG_FILE_PATH = 'MONITORING_CONFIG_FILE_PATH'
+
+
+def get_monitoring_config() -> MonitoringConfig:
+    return get_pipeline_config_for_env_name_and_config_parser(
+        MonitoringEnvironmentVariables.CONFIG_FILE_PATH,
+        MonitoringConfig.from_dict
+    )
+
+
+def monitoring_etl():
+    data_config = get_monitoring_config()
+    ping()
+    run_data_hub_pipeline_health_check(
+        project=data_config.project_name,
+        dataset=data_config.dataset_name,
+        table=data_config.table_name
+    )
+    LOGGER.info('Monitoring Config: %s', data_config)
+
+
 def main():
+    monitoring_etl()
     LOGGER.info('Monitoring pipeline completed successfully.')
 
 
--- data_pipeline/monitoring/monitoring_config.py
+++ data_pipeline/monitoring/monitoring_config.py
@@ -1,23 +1,22 @@
-from data_pipeline.utils.pipeline_config import (
-    update_deployment_env_placeholder
-)
+from dataclasses import dataclass
+import logging
 
 
-# pylint: disable=too-few-public-methods, too-many-instance-attributes
+LOGGER = logging.getLogger(__name__)
+
+
+@dataclass(frozen=True)
 class MonitoringConfig:
+    project_name: str
+    dataset_name: str
+    table_name: str
 
-    def __init__(self, data_config: dict,
-                 deployment_env_var: str,
-                 env_placeholder: str = "{ENV}"
-                 ):
-        self.data_config = update_deployment_env_placeholder(
-            data_config,
-            deployment_env_var,
-            env_placeholder
+    @staticmethod
+    def from_dict(
+        config_dict: dict
+    ) -> 'MonitoringConfig':
+        return MonitoringConfig(
+            project_name=config_dict['project'],
+            dataset_name=config_dict['dataset'],
+            table_name=config_dict['table']
         )
-        self.project_name = self.data_config.get("project")
-        self.dataset_name = self.data_config.get("dataset")
-        self.table_name = self.data_config.get("table")
-
-    def __repr__(self):
-        return repr(vars(self))
