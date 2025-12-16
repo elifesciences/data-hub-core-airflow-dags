c51850533928da7c248e9cf1bb2b928c89ae9b85
Daniel Ecer <de-code@users.noreply.github.com>
2025-12-16 08:22:16 +0000

    Properly declare data_config as dict (and fixed linting issues)

--- data_pipeline/surveymonkey/get_surveymonkey_data_config.py
+++ data_pipeline/surveymonkey/get_surveymonkey_data_config.py
@@ -1,30 +1,27 @@
 # pylint: disable=too-few-public-methods, too-many-instance-attributes
-from typing import Any
-
-
 class SurveyMonkeyDataConfig:
 
-    def __init__(self, data_config: Any):
+    def __init__(self, data_config: dict):
         self.data_config = data_config
         self.project_name = self.data_config.get("projectName")
         self.dataset_name = self.data_config.get("datasetName")
 
         # survey list
         self.survey_list_table_name = (
-            self.data_config.get("surveyMonkeySurveyList").get("table")
+            self.data_config["surveyMonkeySurveyList"].get("table")
         )
         self.survey_id_column_name = (
-            self.data_config.get("surveyMonkeySurveyList").get("survey_id_column_name")
+            self.data_config["surveyMonkeySurveyList"].get("survey_id_column_name")
         )
         # survey ids to request
         self.survey_id_list = self.data_config.get("surveyIdListToRequest")
 
         # survey details
         self.survey_questions_table_name = (
-            self.data_config.get("surveyMonkeySurveyQuestions").get("table")
+            self.data_config["surveyMonkeySurveyQuestions"].get("table")
         )
         self.survey_answers_table_name = (
-            self.data_config.get("surveyMonkeySurveyAnswers").get("table")
+            self.data_config["surveyMonkeySurveyAnswers"].get("table")
         )
 
     def __repr__(self):
