b5d5d1a66e300e490d3ed711593c7cf5841ab8cb
Daniel Ecer <de-code@users.noreply.github.com>
2025-12-16 08:27:46 +0000

    Removed unnessary data_config dict on config object

--- data_pipeline/surveymonkey/get_surveymonkey_data_config.py
+++ data_pipeline/surveymonkey/get_surveymonkey_data_config.py
@@ -2,26 +2,25 @@
 class SurveyMonkeyDataConfig:
 
     def __init__(self, data_config: dict):
-        self.data_config = data_config
-        self.project_name = self.data_config.get('projectName')
-        self.dataset_name = self.data_config.get('datasetName')
+        self.project_name = data_config.get('projectName')
+        self.dataset_name = data_config.get('datasetName')
 
         # survey list
         self.survey_list_table_name = (
-            self.data_config['surveyMonkeySurveyList'].get('table')
+            data_config['surveyMonkeySurveyList'].get('table')
         )
         self.survey_id_column_name = (
-            self.data_config['surveyMonkeySurveyList'].get('survey_id_column_name')
+            data_config['surveyMonkeySurveyList'].get('survey_id_column_name')
         )
         # survey ids to request
-        self.survey_id_list = self.data_config.get('surveyIdListToRequest')
+        self.survey_id_list = data_config.get('surveyIdListToRequest')
 
         # survey details
         self.survey_questions_table_name = (
-            self.data_config['surveyMonkeySurveyQuestions'].get('table')
+            data_config['surveyMonkeySurveyQuestions'].get('table')
         )
         self.survey_answers_table_name = (
-            self.data_config['surveyMonkeySurveyAnswers'].get('table')
+            data_config['surveyMonkeySurveyAnswers'].get('table')
         )
 
     def __repr__(self):
