6386752511de8321aa8d84ef9321d274c31f752b
Daniel Ecer <de-code@users.noreply.github.com>
2025-12-16 09:01:35 +0000

    Added surveymonkey_survey_list_etl step

--- data_pipeline/surveymonkey/cli.py
+++ data_pipeline/surveymonkey/cli.py
@@ -1,8 +1,14 @@
 import argparse
+import json
 import logging
+import os
 from typing import Optional, Sequence
 
 from data_pipeline.surveymonkey.get_surveymonkey_data_config import SurveyMonkeyDataConfig
+from data_pipeline.surveymonkey.surveymonkey_etl import get_survey_list
+from data_pipeline.utils.data_store.bq_data_service import (
+    load_given_json_list_data_from_tempdir_to_bq
+)
 from data_pipeline.utils.pipeline_config import (
     get_pipeline_config_for_env_name_and_config_parser
 )
@@ -13,6 +19,7 @@ LOGGER = logging.getLogger(__name__)
 
 class SurveyMonkeyPipelineEnvironmentVariables:
     CONFIG_FILE_PATH = 'SURVEYMONKEY_DATA_CONFIG_FILE_PATH'
+    SECRET_FILE = 'SURVEYMONKEY_SECRET_FILE'
 
 
 def get_pipeline_config() -> 'SurveyMonkeyDataConfig':
@@ -22,15 +29,41 @@ def get_pipeline_config() -> 'SurveyMonkeyDataConfig':
     )
 
 
+def get_surveymonkey_access_token() -> str:
+    secret_file = os.environ[SurveyMonkeyPipelineEnvironmentVariables.SECRET_FILE]
+    LOGGER.info('surveymonkey secret file name %s', secret_file)
+    with open(secret_file, encoding='UTF-8') as file:
+        return json.load(file)['access_token']
+
+
+def surveymonkey_survey_list_etl(
+    data_config: SurveyMonkeyDataConfig,
+    surveymonkey_access_token: str
+):
+    survey_list = get_survey_list(surveymonkey_access_token)
+    load_given_json_list_data_from_tempdir_to_bq(
+        project_name=data_config.project_name,
+        dataset_name=data_config.dataset_name,
+        table_name=data_config.survey_list_table_name,
+        json_list=survey_list
+    )
+
+
 def main(argv: Optional[Sequence[str]] = None):
     # Name CLI and declare no arguments
-    parser = argparse.ArgumentParser(description="Run ETL for a Survey Monkey pipeline")
+    parser = argparse.ArgumentParser(description='Run ETL for a Survey Monkey pipeline')
     parser.parse_args(argv)
 
     pipeline_config = get_pipeline_config()
     LOGGER.info('pipeline_config: %r', pipeline_config)
 
+    surveymonkey_access_token = get_surveymonkey_access_token()
+
     LOGGER.info('Starting ETL')
+    surveymonkey_survey_list_etl(
+        data_config=pipeline_config,
+        surveymonkey_access_token=surveymonkey_access_token
+    )
     LOGGER.info('ETL process completed successfully.')
 
 
