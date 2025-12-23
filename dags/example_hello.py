f66aaf2be3bc69dad2d1cf89838e57c261fd4540
Daniel Ecer <de-code@users.noreply.github.com>
2025-12-23 09:34:56 +0000

    Removed example DAG

--- dags/example_hello.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime
-
-from airflow.models import DAG
-from airflow.providers.standard.operators.empty import EmptyOperator
-
-
-dag = DAG('example_hello', start_date=datetime(2024, 1, 1))
-t1 = EmptyOperator(task_id='t1', dag=dag)
